8dc2d8762e45d56864befead2de984b73c1af130
hafen <rhafen@gmail.com>
2016-05-10 21:01:36 -0700

    Add RelatedDisplays button/modal

---
+++ src/components/RelatedDisplays.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import Radium from 'radium';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import Mousetrap from 'mousetrap';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import { uiConstsSelector } from '../selectors';
+import { relatedDisplaysSelector } from '../selectors/display';
+
+class RelatedDisplays extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+  componentDidMount() {
+    Mousetrap.bind(['r'], this.handleKey);
+  }
+  componentWillUnmount() {
+    Mousetrap.unbind(['r']);
+  }
+  handleOpen = () => {
+    this.setState({ open: true });
+  }
+  handleKey = () => {
+    this.setState({ open: true });
+  }
+  handleClose = () => {
+    this.setState({ open: false });
+  }
+  render() {
+    const actions = [
+      <FlatButton
+        label="Close"
+        secondary
+        onTouchTap={this.handleClose}
+      />
+    ];
+    return (
+      <div
+        onClick={this.handleOpen}
+        style={this.props.style.button}
+      >
+        <i className="icon-open-add" style={{ paddingLeft: 2, lineHeight: '45px' }}></i>
+        <Dialog
+          title="Add Related Displays"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          {this.props.relatedDisplays.map((d, i) => (
+            <div key={i}>{d.group} / {d.name}</div>
+          ))}
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+RelatedDisplays.propTypes = {
+  style: React.PropTypes.object,
+  selectedDisplay: React.PropTypes.object,
+  relatedDisplays: React.PropTypes.array,
+  handleClick: React.PropTypes.func
+};
+
+// ------ redux container ------
+
+const selectedDisplaySelector = state => state.selectedDisplay;
+
+const styleSelector = createSelector(
+  uiConstsSelector, relatedDisplaysSelector, selectedDisplaySelector,
+  (ui, rd, sd) => ({
+    style: {
+      button: {
+        position: 'fixed',
+        boxSizing: 'border-box',
+        top: 0,
+        transition: 'left 0.5s ease, background 250ms',
+        left: ui.header.height *
+          (sd.name === '' || rd.length === 0 ? 0 : 2),
+        display: 'inline-block',
+        height: ui.header.height,
+        width: ui.header.height,
+        fontSize: 18,
+        lineHeight: `${ui.header.height}px`,
+        color: ui.header.button.color,
+        background: 'white',
+        textAlign: 'center',
+        borderRight: '1px solid',
+        borderBottom: '1px solid',
+        borderColor: ui.header.borderColor,
+        ':hover': {
+          transition: 'background 250ms',
+          background: '#eee',
+          cursor: 'pointer'
+        }
+      }
+    },
+    relatedDisplays: rd
+  })
+);
+
+const mapStateToProps = (state) => (
+  styleSelector(state)
+);
+
+export default connect(
+  mapStateToProps,
+)(Radium(RelatedDisplays));
---
+++ src/selectors/display.js
@@ -0,0 +1,24 @@
+import { createSelector } from 'reselect';
+
+export const displayListSelector = state => state._displayList;
+export const displayInfoSelector = state => state._displayInfo;
+export const currentDisplaySelector = state => state.currentDisplay;
+
+export const relatedDisplaysSelector = createSelector(
+  displayInfoSelector, displayListSelector,
+  (di, dl) => {
+    const res = [];
+    if (di.isLoaded) {
+      const keySig = di.info.keySig;
+      const dispID = [di.info.group, di.info.name].join('/');
+      for (let i = 0; i < dl.list.length; i++) {
+        const sameKey = dl.list[i].keySig === keySig;
+        const curID = [dl.list[i].group, dl.list[i].name].join('/');
+        if (sameKey && curID !== dispID) {
+          res.push({ name: dl.list[i].name, group: dl.list[i].group });
+        }
+      }
+    }
+    return res;
+  }
+);
