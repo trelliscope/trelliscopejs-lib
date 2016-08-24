a6129c4dcd09ce7fd42e9851464a109e03bdddc7
hafen <rhafen@gmail.com>
2016-08-24 13:52:54 -0700

    Add informational icons and text info to footer chips

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,22 +6,24 @@ import { filterCardinalitySelector } from '../selectors/cogData.js';
 import { displayInfoSelector, filterSelector, sortSelector } from '../selectors';
 import FooterChip from './FooterChip';
 
-const Footer = ({ style, sortNames, filterNames, nFilt, nPanels }) => {
+const Footer = ({ style, sort, filter, nFilt, nPanels }) => {
   let sortContent = '';
   let filterContent = '';
   let spacerContent = '';
 
-  if (sortNames.length > 0) {
+  if (sort.length > 0) {
     sortContent = (
       <div style={style.sectionWrapper}>
         <div style={style.sectionText}>
           Sorting on:
         </div>
         <div style={style.chipWrapper}>
-          {sortNames.map((el, i) => (
+          {sort.map((el, i) => (
             <FooterChip
-              key={`${el}_sortchip`}
-              label={el}
+              key={`${el.name}_sortchip`}
+              label={el.name}
+              icon={el.icon}
+              text={''}
               index={i}
               type="sort"
             />
@@ -31,17 +33,19 @@ const Footer = ({ style, sortNames, filterNames, nFilt, nPanels }) => {
     );
   }
 
-  if (filterNames.length > 0) {
+  if (filter.length > 0) {
     filterContent = (
       <div style={style.sectionWrapper}>
         <div style={style.sectionText}>
           Filtering on:
         </div>
         <div style={style.chipWrapper}>
-          {filterNames.map((el, i) => (
+          {filter.map((el, i) => (
             <FooterChip
-              key={`${el}_filterchip`}
-              label={el}
+              key={`${el.name}_filterchip`}
+              label={el.name}
+              icon={''}
+              text={el.text}
               index={i}
               type="filter"
             />
@@ -54,7 +58,7 @@ const Footer = ({ style, sortNames, filterNames, nFilt, nPanels }) => {
     );
   }
 
-  if (filterNames.length > 0 && sortNames.length > 0) {
+  if (filter.length > 0 && sort.length > 0) {
     spacerContent = <div style={style.spacer} />;
   }
 
@@ -69,43 +73,80 @@ const Footer = ({ style, sortNames, filterNames, nFilt, nPanels }) => {
 
 Footer.propTypes = {
   style: React.PropTypes.object,
-  sortNames: React.PropTypes.array,
-  filterNames: React.PropTypes.array,
+  sort: React.PropTypes.array,
+  filter: React.PropTypes.array,
   nFilt: React.PropTypes.number,
   nPanels: React.PropTypes.number
 };
 
 // ------ redux container ------
 
-const sortNamesSelector = createSelector(
-  sortSelector,
-  (sort) => {
-    const names = [];
+const sortInfoSelector = createSelector(
+  sortSelector, displayInfoSelector,
+  (sort, di) => {
+    const res = [];
     for (let i = 0; i < sort.length; i++) {
-      names.push(sort[i].name);
+      const name = sort[i].name;
+      const type = di.info.cogInfo[name].type;
+      let icon = 'icon-sort-amount';
+      if (type === 'factor') {
+        icon = 'icon-sort-alpha';
+      } else if (type === 'numeric') {
+        icon = 'icon-sort-numeric';
+      }
+      icon = `${icon}-${sort[i].dir}`;
+      res.push({ name, icon });
     }
-    return names;
+    return res;
   }
 );
 
-const filterNamesSelector = createSelector(
-  filterSelector,
-  filter => {
+const filterInfoSelector = createSelector(
+  filterSelector, displayInfoSelector,
+  (filter, di) => {
     const keys = Object.keys(filter.state);
-    const activeKeys = [];
+    const res = [];
     for (let i = 0; i < keys.length; i++) {
-      if (filter.state[keys[i]].value !== undefined) {
-        activeKeys.push(keys[i]);
+      const curState = filter.state[keys[i]];
+      if (curState.value !== undefined) {
+        let text = '';
+        if (curState.varType === 'numeric') {
+          if (curState.value.from === undefined && curState.value.to !== undefined) {
+            text = `< ${curState.value.to}`;
+          } else if (curState.value.from !== undefined && curState.value.to === undefined) {
+            text = `> ${curState.value.from}`;
+          } else if (curState.value.from !== undefined && curState.value.to !== undefined) {
+            text = `${curState.value.from} \u2013 ${curState.value.to}`;
+          }
+        } else if (curState.varType === 'factor') {
+          const charLimit = 15;
+          const n = curState.value.length;
+          let textLength = 0;
+          let idx = 0;
+          while (idx < n && textLength <= charLimit) {
+            textLength = textLength + curState.value[idx].length + 2;
+            idx++;
+          }
+          if (idx === n) {
+            // build a string of selected values
+            text = curState.value.sort().join(', ');
+          } else {
+            // just show "k of n"
+            const tot = di.info.cogInfo[curState.name].levels.length;
+            text = `${curState.value.length} of ${tot}`;
+          }
+        }
+        res.push({ name: keys[i], text });
       }
     }
-    return activeKeys;
+    return res;
   }
 );
 
 const styleSelector = createSelector(
-  windowWidthSelector, uiConstsSelector, sortNamesSelector, filterNamesSelector,
+  windowWidthSelector, uiConstsSelector, sortInfoSelector, filterInfoSelector,
   filterCardinalitySelector, displayInfoSelector,
-  (ww, ui, sortNames, filterNames, nFilt, di) => ({
+  (ww, ui, sort, filter, nFilt, di) => ({
     style: {
       wrapper: {
         position: 'absolute',
@@ -142,8 +183,8 @@ const styleSelector = createSelector(
         marginLeft: 5
       }
     },
-    sortNames,
-    filterNames,
+    sort,
+    filter,
     nFilt,
     nPanels: di.info.n
   })
--- src/components/FooterChip.js
+++ src/components/FooterChip.js
@@ -6,30 +6,45 @@ import { createSelector } from 'reselect';
 import { uiConstsSelector } from '../selectors/ui';
 import { setFilterView, setFilter, setLayout, setSort } from '../actions';
 
-const FooterChip = ({ label, index, style, type, handleStateClose }) => (
-  <div type="button" style={style.wrapper} key="label">
-    <span style={style.label}>
-      {label}
-    </span>
-    <svg
-      viewBox="0 0 24 24"
-      style={style.icon}
-      key="icon"
-      onClick={() => handleStateClose({ label, index, type })}
-    >
-      <path
-        d={
-          'M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 ' +
-          '13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 ' +
-          '10.59 15.59 7 17 8.41 13.41 12 17 15.59z'
-        }
-      />
-    </svg>
-  </div>
-);
+const FooterChip = ({ label, icon, text, index, style, type, handleStateClose }) => {
+  let iconTag = '';
+  if (icon !== '') {
+    iconTag = <i className={icon} style={style.indIcon} />;
+  }
+  let textTag = '';
+  if (text !== '') {
+    textTag = <span style={style.text}>({text})</span>;
+  }
+
+  return (
+    <div type="button" style={style.wrapper}>
+      <span style={style.label}>
+        {iconTag}
+        {label}
+        {textTag}
+      </span>
+      <svg
+        viewBox="0 0 24 24"
+        style={style.closeIcon}
+        key="icon"
+        onClick={() => handleStateClose({ label, index, type })}
+      >
+        <path
+          d={
+            'M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 ' +
+            '13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 ' +
+            '10.59 15.59 7 17 8.41 13.41 12 17 15.59z'
+          }
+        />
+      </svg>
+    </div>
+  );
+};
 
 FooterChip.propTypes = {
   label: React.PropTypes.string,
+  icon: React.PropTypes.string,
+  text: React.PropTypes.string,
   index: React.PropTypes.number,
   style: React.PropTypes.object,
   type: React.PropTypes.string,
@@ -55,7 +70,8 @@ const styleSelector = createSelector(
         whiteSpace: 'nowrap',
         width: '-webkit-fit-content',
         background: ui.footer.button.background,
-        height: 22
+        height: 22,
+        transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'
         // cursor: 'pointer',
         // transition: 'all 150ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
         // ':hover': {
@@ -72,7 +88,12 @@ const styleSelector = createSelector(
         whiteSpace: 'nowrap',
         WebkitUserSelect: 'none'
       },
-      icon: {
+      text: {
+        paddingLeft: 5,
+        fontSize: 11,
+        fontStyle: 'italic'
+      },
+      closeIcon: {
         display: 'inline-block',
         color: 'rgba(0, 0, 0, 0.22)',
         fill: 'rgba(0, 0, 0, 0.22)',
@@ -86,6 +107,9 @@ const styleSelector = createSelector(
           color: fade('rgba(0, 0, 0, 0.22)', 0.4),
           fill: fade('rgba(0, 0, 0, 0.22)', 0.4)
         }
+      },
+      indIcon: {
+        paddingRight: 5
       }
     },
     ui
