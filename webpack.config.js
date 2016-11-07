98f4d67fc867256ed21fabf06b465f7da72a7206
hafen <rhafen@gmail.com>
2016-11-07 14:19:34 -0800

    Update output file to 'trelliscope.js'

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'trelliscope.js'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
--- webpack.min.config.js
+++ webpack.min.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'trelliscope.min.js'
   },
   plugins: [
     new webpack.DefinePlugin({
