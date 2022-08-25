9e1a107acb1df4b90a0b025ee8d32c3c114f691c
David Enciso <dav@Davids-MacBook-Pro.local>
2022-08-25 09:21:16 -0600

    55 css module for styles

--- src/components/HeaderLogo/HeaderLogo.js
+++ src/components/HeaderLogo/HeaderLogo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import injectSheet from 'react-jss';
 import { connect } from 'react-redux';
+import classNames from 'classnames';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -10,11 +10,10 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Mousetrap from 'mousetrap';
 import Button from '@material-ui/core/Button';
-import { emphasize } from '@material-ui/core/styles/colorManipulator';
 import { createSelector } from 'reselect';
 import { fullscreenSelector } from '../../selectors';
 import { windowHeightSelector } from '../../selectors/ui';
-import uiConsts from '../../assets/styles/uiConsts';
+import styles from './HeaderLogo.module.scss';
 
 class HeaderLogo extends React.Component {
   constructor(props) {
@@ -71,28 +70,28 @@ class HeaderLogo extends React.Component {
   };
 
   render() {
-    const { classes, fullscreen, windowHeight } = this.props;
+    const { fullscreen,  windowHeight } = this.props;
 
     let keyNote = '';
     if (!fullscreen) {
-      keyNote = <p className={classes.keynote}>Note: keyboard shortcuts are only available when the app is fullscreen.</p>;
+      keyNote = <p className={styles.headerLogoKeynote}>Note: keyboard shortcuts are only available when the app is fullscreen.</p>;
     }
 
     const container1 = (
-      <div className={classes.dialogDiv} style={{ maxHeight: Math.max(50, windowHeight - 310) }}>
-        <div className={classes.dialogP}>
+      <div className={styles.headerLogoDialogDiv} style={{ maxHeight: Math.max(50, windowHeight - 310) }}>
+        <div className={styles.headerLogoDialogP}>
           <strong>What: </strong>
           Trelliscope is a tool for interactively viewing a large collection of visualizations. Each visualization in a
           collection is called a&nbsp;
           <em>panel</em>
           &nbsp;and each panel typically represents one slice of a large dataset.
         </div>
-        <div className={classes.dialogP}>
+        <div className={styles.headerLogoDialogP}>
           <strong>Why: </strong>
           Viewing multiple slices of a dataset simultaneously is a simple but very powerful visual technique and provides a
           way to visualize data in greater detail, particularly when the dataset is large.
         </div>
-        <div className={classes.dialogP}>
+        <div className={styles.headerLogoDialogP}>
           <strong>Interactivity: </strong>
           When there are many panels, it is useful to be able to navigate to which panels you want to view and make
           panel-to-panel comparisons by sorting and filtering the panels based on various criteria. Trelliscope provides this
@@ -110,15 +109,15 @@ class HeaderLogo extends React.Component {
           ,&nbsp;and&nbsp;
           <strong>Sort</strong>.
         </p>
-        <div className={classes.dialogP}>
-          <i className={`icon-th ${classes.dialogHi}`} />
+        <div className={styles.headerLogoDialogP}>
+          <i className={classNames('icon-th', styles.headerLogoDialogHi)} />
           &nbsp;
           <strong>Grid: </strong>
           In the &quot;Grid&quot; sidebar, you can specify the layout of the grid of panels you wish to display, specifying
           the number of rows and columns of the grid, as well as whether to arrange panels in order by row or by column.
         </div>
-        <div className={classes.dialogP}>
-          <i className={`icon-list-ul ${classes.dialogHi}`} />
+        <div className={styles.headerLogoDialogP}>
+          <i className={classNames('icon-list-ul', styles.headerLogoDialogHi)} />
           &nbsp;
           <strong>Labels: </strong>
           In the &quot;Labels&quot; sidebar, you can specify the panel metrics that you wish to see displayed under each
@@ -127,14 +126,14 @@ class HeaderLogo extends React.Component {
           view and clicking the &quot;x&quot; button that appears. Labels are also automatically added when you specify a new
           variable to sort or filter on.
         </div>
-        <div className={classes.dialogP}>
-          <i className={`icon-filter ${classes.dialogHi}`} />
+        <div className={styles.headerLogoDialogP}>
+          <i className={classNames('icon-filter', styles.headerLogoDialogHi)} />
           &nbsp;
           <strong>Filter: </strong>
           The &quot;Filter&quot; sidebar provides various ways to filter the panels being displayed based on the panel
           metrics. A list of variables is available as buttons. Clicking a button will produce a visual distribution of the
           variable.
-          <p className={classes.dialogP2}>
+          <p className={styles.headerLogoDialogP2}>
             <strong>Categorical filter: </strong>
             For categorical variables, a bar chart is provided, showing the the possible values of the variable with the size
             of the bars relating to the count of panels available under the current filtering state of all other variables. A
@@ -149,29 +148,29 @@ class HeaderLogo extends React.Component {
             </a>
             .
           </p>
-          <p className={classes.dialogP2}>
+          <p className={styles.headerLogoDialogP2}>
             <strong>Numeric filter: </strong>
             For numeric variables, a histogram is provided, which shows the distribution of the variable based on all other
             active filters. You can click and drag left to right to highlight a range of the variable for which you would
             like to filter the panels. Alternatively, you can manually enter a range in the fields provided below the
             histogram.
           </p>
-          <p className={classes.dialogP22}>
+          <p className={styles.headerLogoDialogP22}>
             To reset a filter variable, you can either clear out the fields, deselect the selections made in the distribution
             plots, or click the&nbsp;
             <i className="icon-undo" />
             &nbsp;button located at the top right of the filter box for that variable. This icon is only available if there
             is an active filter for the variable.
           </p>
-          <p className={classes.dialogP22}>
+          <p className={styles.headerLogoDialogP22}>
             To close a filter box, click the&nbsp;
             <i className="icon-times-circle" />
             &nbsp;button located at the top right of the filter box. Note that if there is an active filter on the variable,
             its button in the &quot;More variables&quot; section will be green to indicate this.
           </p>
         </div>
-        <div className={classes.dialogP}>
-          <i className={`icon-sort-amount-asc ${classes.dialogHi}`} />
+        <div className={styles.headerLogoDialogP}>
+        <i className={classNames('icon-sort-amount-asc', styles.headerLogoDialogHi)} />
           &nbsp;
           <strong>Sort: </strong>
           In the &quot;Sort&quot; sidebar, a list of variables which are currently being sorted by (if any) will be listed at
@@ -180,7 +179,7 @@ class HeaderLogo extends React.Component {
           tertiary, etc. sorting. For an active sortig variable, clicking the blue icon with an arrow pointing up or down
           will change the order of the sorting.
         </div>
-        <div className={classes.dialogP}>
+        <div className={styles.headerLogoDialogP}>
           The active filter and sort state are displayed at the bottom of the page in the footer. Clicking the&nbsp;
           <i className="icon-times-circle" />
           &nbsp;button for anything listed in the footer will remove the sorting or filtering on that variable.
@@ -189,70 +188,70 @@ class HeaderLogo extends React.Component {
     );
 
     const container2 = (
-      <div className={classes.dialogDiv}>
+      <div className={styles.headerLogoDialogDiv}>
         <div>
           {keyNote}
           <div style={{ width: '50%', display: 'block', float: 'left' }}>
-            <h4 className={classes.dialogH4}>Sidebar controls</h4>
-            <ul className={classes.dialogUl}>
+            <h4 className={styles.headerLogoDialogH4}>Sidebar controls</h4>
+            <ul className={styles.headerLogoDialogUl}>
               <li>
-                <code className={classes.dialogCode}>g</code>
+                <code className={styles.headerLogoDialogCode}>g</code>
                 &ensp;open &quot;Grid&quot; sidebar
               </li>
               <li>
-                <code className={classes.dialogCode}>l</code>
+                <code className={styles.headerLogoDialogCode}>l</code>
                 &ensp;open &quot;Labels&quot; sidebar
               </li>
               <li>
-                <code className={classes.dialogCode}>f</code>
+                <code className={styles.headerLogoDialogCode}>f</code>
                 &ensp;open &quot;Filter&quot; sidebar
               </li>
               <li>
-                <code className={classes.dialogCode}>s</code>
+                <code className={styles.headerLogoDialogCode}>s</code>
                 &ensp;open &quot;Sort&quot; sidebar
               </li>
               <li>
-                <code className={classes.dialogCode}>esc</code>
+                <code className={styles.headerLogoDialogCode}>esc</code>
                 &ensp;close sidebar
               </li>
             </ul>
-            <h4 className={classes.dialogH4}>Panel navigation</h4>
-            <ul className={classes.dialogUl}>
+            <h4 className={styles.headerLogoDialogH4}>Panel navigation</h4>
+            <ul className={styles.headerLogoDialogUl}>
               <li>
-                <code className={classes.dialogCode}>left</code>
+                <code className={styles.headerLogoDialogCode}>left</code>
                 &ensp;page back
               </li>
               <li>
-                <code className={classes.dialogCode}>right</code>
+                <code className={styles.headerLogoDialogCode}>right</code>
                 &ensp;page forward
               </li>
             </ul>
           </div>
           <div style={{ width: '50%', display: 'block', float: 'left' }}>
-            <h4 className={classes.dialogH4}>Dialog boxes</h4>
-            <ul className={classes.dialogUl}>
+            <h4 className={styles.headerLogoDialogH4}>Dialog boxes</h4>
+            <ul className={styles.headerLogoDialogUl}>
               <li>
-                <code className={classes.dialogCode}>i</code>
+                <code className={styles.headerLogoDialogCode}>i</code>
                 &ensp;open &quot;Display Info&quot; dialog
               </li>
               <li>
-                <code className={classes.dialogCode}>a</code>
+                <code className={styles.headerLogoDialogCode}>a</code>
                 &ensp;open &quot;About&quot; dialog
               </li>
               <li>
-                <code className={classes.dialogCode}>esc</code>
+                <code className={styles.headerLogoDialogCode}>esc</code>
                 &ensp;close dialog
               </li>
             </ul>
-            <h4 className={classes.dialogH4}>Touchscreen devices</h4>
-            <p className={classes.dialogUl}>Swiping left and right will page the panels forward and backward</p>
+            <h4 className={styles.headerLogoDialogH4}>Touchscreen devices</h4>
+            <p className={styles.headerLogoDialogUl}>Swiping left and right will page the panels forward and backward</p>
           </div>
         </div>
       </div>
     );
 
     const container3 = (
-      <div className={classes.dialogDiv}>
+      <div className={styles.headerLogoDialogDiv}>
         <p>
           &copy;&nbsp;
           <a href="http://ryanhafen.com" target="_blank" rel="noopener noreferrer">
@@ -294,10 +293,10 @@ class HeaderLogo extends React.Component {
 
     return (
       <div>
-        <button type="button" onClick={this.handleOpen} className={classes.logo}>
+        <button type="button" onClick={this.handleOpen} className={styles.headerLogo}>
           Trelliscope
-          <div className={classes.icon}>
-            <i className={`icon-help ${classes.icon}`} />
+          <div className={styles.headerLogoIcon}>
+            <i className={classNames('icon-help', styles.headerLogoIcon)} />
           </div>
         </button>
         <Dialog
@@ -330,78 +329,9 @@ class HeaderLogo extends React.Component {
 }
 
 HeaderLogo.propTypes = {
-  classes: PropTypes.object.isRequired,
   windowHeight: PropTypes.number.isRequired,
   fullscreen: PropTypes.bool.isRequired,
   setDialogOpen: PropTypes.func.isRequired,
-};
-
-// ------ static styles ------
-
-const staticStyles = {
-  logo: {
-    position: 'absolute',
-    top: -1, // cover up top app border
-    right: 0,
-    cursor: 'pointer',
-    border: 'none',
-    // borderColor: uiConsts.header.borderColor,
-    height: uiConsts.header.height,
-    textAlign: 'center',
-    width: uiConsts.header.logoWidth,
-    fontSize: 17,
-    background: uiConsts.header.logo.background,
-    color: uiConsts.header.logo.color,
-  },
-  icon: {
-    position: 'absolute',
-    top: 1,
-    right: 1,
-    color: emphasize(uiConsts.header.logo.background, 0.4),
-  },
-  dialogHeadline: {
-    fontSize: 24,
-    paddingTop: 16,
-    marginBottom: 12,
-    fontWeight: 400,
-  },
-  dialogDiv: {
-    fontSize: 18,
-    paddingLeft: 10,
-    paddingRight: 10,
-    zIndex: 5000,
-  },
-  dialogH4: {
-    marginBottom: 5,
-  },
-  dialogUl: {
-    marginTop: 0,
-    breakInside: 'avoid-column',
-  },
-  dialogCode: {
-    color: '#FF5252',
-    fontSize: 20,
-  },
-  dialogP: {
-    textIndent: -20,
-    paddingLeft: 20,
-    marginTop: '1em',
-  },
-  dialogP2: {
-    paddingLeft: 20,
-  },
-  dialogP22: {
-    textIndent: 0,
-    paddingLeft: 20,
-  },
-  dialogHi: {
-    color: '#FF5252',
-  },
-  keynote: {
-    marginBottom: 0,
-    color: '#888',
-    fontStyle: 'italic',
-  },
 };
 
 // ------ redux container ------
@@ -413,4 +343,4 @@ const styleSelector = createSelector(windowHeightSelector, fullscreenSelector, (
 
 const mapStateToProps = (state) => styleSelector(state);
 
-export default connect(mapStateToProps)(injectSheet(staticStyles)(HeaderLogo));
+export default connect(mapStateToProps)(HeaderLogo);
