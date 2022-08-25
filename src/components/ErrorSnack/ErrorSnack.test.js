3a870968a263ca36be88850dc75e14a8bbee4148
Brandon McArthur <brandon_mcarthur@msn.com>
2022-08-25 13:17:57 -0600

    59: errorsnack conversion complete (#74)
    * 59: errorsnack conversion complete
    
    * 59: added airbnb typescript support
    
    * 59: updated import
    
    Co-authored-by: Brandon McArthur <brandonmcarthur@Brandons-MacBook-Pro-3.local>

---
+++ src/components/ErrorSnack/ErrorSnack.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { configureStore } from 'redux-mock-store';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import ErrorSnack from './ErrorSnack';
+
+const mockStore = configureStore([]);
+
+describe('ErrorSnack Component', () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      errorMsg: 'test error message',
+    });
+  });
+  test('should render without crashing', () => {
+    render(
+      <Provider store={store}>
+        <ErrorSnack errorMsg="" handleClose={() => {}} />
+      </Provider>,
+    );
+    expect(screen.getByText('Close')).toBeInTheDocument();
+  });
+
+  test('should render with supplied error message', () => {
+    render(
+      <Provider store={store}>
+        <ErrorSnack errorMsg="" handleClose={() => {}} />
+      </Provider>,
+    );
+    expect(screen.getByText('test error message')).toBeInTheDocument();
+  });
+
+  // TODO this needs to be moved to an integration test once the framework is up, it wont work here due to the mock not updating the reducer state.
+  //   test('should close and remove error message', async () => {
+  //     render(
+  //       <Provider store={store}>
+  //         <ErrorSnack errorMsg="" handleClose={() => {}} />
+  //       </Provider>,
+  //     );
+  //     expect(screen.getByText('test error message')).toBeInTheDocument();
+  //     const closeButton = await waitFor(() => screen.getByText('Close'));
+  //     fireEvent.click(closeButton);
+  //     expect(screen.getByText('test error message')).not.toBeInTheDocument();
+  //   });
+});
--- src/components/ErrorSnack/ErrorSnack.tsx
+++ src/components/ErrorSnack/ErrorSnack.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
+import { Action, Dispatch } from 'redux';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import Snackbar from '@material-ui/core/Snackbar';
 import Button from '@material-ui/core/Button';
+import { setErrorMessage } from '../../actions';
 
-import { setErrorMessage } from '../actions';
+interface ErrorSnackProps {
+  errorMsg: string;
+  handleClose: () => void;
+}
 
-const ErrorSnack = ({ errorMsg, handleClose }) => (
+const ErrorSnack: React.FC<ErrorSnackProps> = ({ errorMsg, handleClose }) => (
   <Snackbar
     anchorOrigin={{
       vertical: 'bottom',
       horizontal: 'center',
     }}
     open={errorMsg !== ''}
-    // autoHideDuration={6000}
     onClose={handleClose}
-    SnackbarContentProps={{
-      'aria-describedby': 'message-id',
-    }}
-    message={<span id="message-id">{errorMsg}</span>}
+    message={errorMsg}
     action={[
-      <Button key="undo" color="secondary" dense onClick={handleClose}>
+      <Button key="undo" color="secondary" size="small" onClick={handleClose}>
         Close
       </Button>,
     ]}
@@ -33,23 +34,17 @@ ErrorSnack.propTypes = {
   handleClose: PropTypes.func.isRequired,
 };
 
-// ------ static styles ------
-
-// const staticStyles = {
-//   overlay: {}
-// };
-
 // ------ redux container ------
 
-const errorSelector = (state) => state.errorMsg;
+const errorSelector = (state: { errorMsg: string }) => state.errorMsg;
 
 const stateSelector = createSelector(errorSelector, (errorMsg) => ({
   errorMsg,
 }));
 
-const mapStateToProps = (state) => stateSelector(state);
+const mapStateToProps = (state: { errorMsg: string }) => stateSelector(state);
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = (dispatch: Dispatch<Action>) => ({
   handleClose: () => {
     dispatch(setErrorMessage(''));
   },
