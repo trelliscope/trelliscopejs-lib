47a327f0a097f49676607b6b8954c78c8807d244
dpenciso <dpenciso@msn.com>
2022-08-29 11:37:17 -0600

    fixed import statement

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import DisplayInfo from './DisplayInfo';
 import RelatedDisplays from './RelatedDisplays';
 import DisplaySelect from './DisplaySelect';
 import Pagination from './Pagination';
-import HeaderLogo from './HeaderLogo/HeaderLogo';
+import HeaderLogo from './HeaderLogo';
 import { setSelectedDisplay, fetchDisplay, setDialogOpen } from '../actions';
 import { windowWidthSelector } from '../selectors/ui';
 import { relatedDisplayGroupsSelector, displayGroupsSelector } from '../selectors/display';
