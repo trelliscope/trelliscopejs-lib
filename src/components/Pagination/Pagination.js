0e32d12561c3de1aac4a4747848b22631f1b6c77
dpenciso <dpenciso@msn.com>
2022-08-29 14:32:27 -0600

    functionalization

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
@@ -13,59 +13,8 @@ import { nPerPageSelector, pageNumSelector, dialogOpenSelector, fullscreenSelect
 import { filterCardinalitySelector } from '../../selectors/cogData';
 import styles from './Pagination.module.scss';
 
-class Pagination extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   // this.state = { skip: 1 };
-  // }
-  componentDidMount() {
-    const { fullscreen, dialogOpen } = this.props;
-    if (fullscreen) {
-      Mousetrap.bind('right', () => {
-        if (!dialogOpen) {
-          this.pageRight();
-        }
-      });
-      Mousetrap.bind('left', () => {
-        if (!dialogOpen) {
-          this.pageLeft();
-        }
-      });
-    }
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    // eslint-disable-line camelcase
-    const { dialogOpen } = nextProps;
-    if (nextProps.fullscreen) {
-      Mousetrap.bind('right', () => {
-        if (!dialogOpen) {
-          this.pageRight();
-        }
-      });
-      Mousetrap.bind('left', () => {
-        if (!dialogOpen) {
-          this.pageLeft();
-        }
-      });
-    } else {
-      Mousetrap.unbind(['right', 'left']);
-    }
-  }
-
-  // shouldComponentUpdate(nextProps) {
-  //   return nextProps.cogData.isLoaded && nextProps.cogData.crossfilter !== undefined;
-  // }
-
-  componentWillUnmount() {
-    const { fullscreen } = this.props;
-    if (fullscreen) {
-      Mousetrap.unbind(['right', 'left']);
-    }
-  }
-
-  pageLeft = () => {
-    const { n, handleChange } = this.props;
+const Pagination = ({ n, npp, totPages, totPanels, dialogOpen, fullscreen, handleChange, cogData }) => {
+  const pageLeft = () => {
     let nn = n - 1;
     if (nn < 1) {
       nn += 1;
@@ -73,8 +22,7 @@ class Pagination extends React.Component {
     return handleChange(nn);
   };
 
-  pageRight = () => {
-    const { n, totPages, handleChange } = this.props;
+  const pageRight = () => {
     let nn = n + 1;
     if (nn > totPages) {
       nn -= 1;
@@ -82,105 +30,94 @@ class Pagination extends React.Component {
     return handleChange(nn);
   };
 
-  pageFirst = () => {
-    const { handleChange } = this.props;
+  const pageFirst = () => {
     handleChange(1);
   };
 
-  pageLast = () => {
-    const { handleChange, totPages } = this.props;
+  const pageLast = () => {
     handleChange(totPages);
   };
 
-  render() {
-    const { cogData, totPages, totPanels, npp, n } = this.props;
+  useEffect(() => {
+    if (fullscreen) {
+      Mousetrap.bind('right', () => {
+        if (!dialogOpen) {
+          pageRight();
+        }
+      });
+      Mousetrap.bind('left', () => {
+        if (!dialogOpen) {
+          pageLeft();
+        }
+      });
+    }
+  }, []);
 
-    if (cogData.isFetching || (cogData.isLoaded && cogData.crossfilter === undefined)) {
-      return <div className={styles.paginationProgress}>loading panels...</div>;
-    }
-    if (totPanels === 0) {
-      return <div />;
-    }
+  if (cogData.isFetching || (cogData.isLoaded && cogData.crossfilter === undefined)) {
+    return <div className={styles.paginationProgress}>loading panels...</div>;
+  }
+  if (totPanels === 0) {
+    return <div />;
+  }
 
-    const pFrom = npp * (n - 1) + 1;
-    const pTo = Math.min(npp * n, totPanels);
-    let pRange = <span>{pFrom}</span>;
-    if (pFrom !== pTo) {
-      pRange = (
-        <span>
-          {pFrom}
-          &nbsp;
-          <span className={styles.paginationPageDash}>-</span>
-          &nbsp;
-          {pTo}
-        </span>
-      );
-    }
-    const txt = (
+  const pFrom = npp * (n - 1) + 1;
+  const pTo = Math.min(npp * n, totPanels);
+  let pRange = <span>{pFrom}</span>;
+  if (pFrom !== pTo) {
+    pRange = (
       <span>
-        {pRange}
-        <span>{` of ${totPanels}`}</span>
+        {pFrom}
+        &nbsp;
+        <span className={styles.paginationPageDash}>-</span>
+        &nbsp;
+        {pTo}
       </span>
     );
-    return (
-      <div className={styles.paginationOuter}>
-        <div className={styles.paginationLabel}>{txt}</div>
-        <div className={styles.paginationButtonWrap}>
-          <div className={styles.paginationButtonDiv}>
-            <IconButton
-              disabled={n <= 1}
-              className={styles.paginationButton}
-              // iconStyle={styles.icon}
-              onClick={() => this.pageFirst()}
-            >
-              <FirstPageIcon />
-            </IconButton>
-          </div>
-          <div className={styles.paginationButtonText}>First</div>
-        </div>
-        <div className={styles.paginationButtonWrap}>
-          <div className={styles.paginationButtonDiv}>
-            <IconButton
-              disabled={n <= 1}
-              className={styles.paginationButton}
-              // iconStyle={styles.icon}
-              onClick={() => this.pageLeft()}
-            >
-              <ChevronLeftIcon />
-            </IconButton>
-          </div>
-          <div className={styles.paginationButtonText}>Prev</div>
-        </div>
-        <div className={styles.paginationButtonWrap}>
-          <div className={styles.paginationButtonDiv}>
-            <IconButton
-              disabled={n >= totPages}
-              className={styles.paginationButton}
-              // iconStyle={styles.icon}
-              onClick={() => this.pageRight()}
-            >
-              <ChevronRightIcon />
-            </IconButton>
-          </div>
-          <div className={styles.paginationButtonText}>Next</div>
-        </div>
-        <div className={styles.paginationButtonWrap}>
-          <div className={styles.paginationButtonDiv}>
-            <IconButton
-              disabled={n >= totPages}
-              className={styles.paginationButton}
-              // iconStyle={styles.icon}
-              onClick={() => this.pageLast()}
-            >
-              <LastPageIcon />
-            </IconButton>
-          </div>
-          <div className={styles.paginationButtonText}>Last</div>
-        </div>
-      </div>
-    );
   }
-}
+  const txt = (
+    <span>
+      {pRange}
+      <span>{` of ${totPanels}`}</span>
+    </span>
+  );
+  return (
+    <div className={styles.paginationOuter}>
+      <div className={styles.paginationLabel}>{txt}</div>
+      <div className={styles.paginationButtonWrap}>
+        <div className={styles.paginationButtonDiv}>
+          <IconButton disabled={n <= 1} className={styles.paginationButton} onClick={() => pageFirst()}>
+            <FirstPageIcon />
+          </IconButton>
+        </div>
+        <div className={styles.paginationButtonText}>First</div>
+      </div>
+      <div className={styles.paginationButtonWrap}>
+        <div className={styles.paginationButtonDiv}>
+          <IconButton disabled={n <= 1} className={styles.paginationButton} onClick={() => pageLeft()}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <div className={styles.paginationButtonText}>Prev</div>
+      </div>
+      <div className={styles.paginationButtonWrap}>
+        <div className={styles.paginationButtonDiv}>
+          <IconButton disabled={n >= totPages} className={styles.paginationButton} onClick={() => pageRight()}>
+            <ChevronRightIcon />
+          </IconButton>
+        </div>
+        <div className={styles.paginationButtonText}>Next</div>
+      </div>
+      <div className={styles.paginationButtonWrap}>
+        <div className={styles.paginationButtonDiv}>
+          <IconButton disabled={n >= totPages} className={styles.paginationButton} onClick={() => pageLast()}>
+            <LastPageIcon />
+          </IconButton>
+        </div>
+        <div className={styles.paginationButtonText}>Last</div>
+      </div>
+    </div>
+  );
+};
 
 Pagination.propTypes = {
   n: PropTypes.number.isRequired,
