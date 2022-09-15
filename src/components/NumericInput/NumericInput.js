7bc51d95daf2c05e31ae16277182a46091627b73
dpenciso <dpenciso@msn.com>
2022-09-15 10:53:29 -0600

    css modules

--- src/components/NumericInput/NumericInput.js
+++ src/components/NumericInput/NumericInput.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import injectSheet from 'react-jss';
 import Mousetrap from 'mousetrap';
-import uiConsts from '../../assets/styles/uiConsts';
+import styles from './NumericInput.module.scss';
 
 class NumericInput extends React.Component {
   constructor(props) {
@@ -81,7 +81,7 @@ class NumericInput extends React.Component {
   };
 
   render() {
-    const { classes, arrows, size } = this.props;
+    const { arrows, size } = this.props;
     const { value } = this.state;
 
     let arrowElements = '';
@@ -91,34 +91,34 @@ class NumericInput extends React.Component {
           <button
             type="button"
             key="up-button"
-            className={`${classes.b} ${classes.b1}`}
+            className={styles.numericInputB}
             onClick={this.increment}
             tabIndex="-1"
           >
-            <i className={`${classes.i} ${classes.i1}`} />
+            <i className={styles.numericInputI} />
           </button>
           <button
             type="button"
             key="down-button"
-            className={`${classes.b} ${classes.b2}`}
+            className={styles.numericInputB2}
             onClick={this.decrement}
             tabIndex="-1"
           >
-            <i className={`${classes.i} ${classes.i2}`} />
+            <i className={styles.numericInputI2} />
           </button>
         </span>
       );
     }
 
     return (
-      <span className={classes.span}>
+      <span className={styles.numericInputSpan}>
         <input
           ref={(d) => {
             this._NumericInput = d;
           }}
           type="text"
           size={size ? size : 4} // eslint-disable-line no-unneeded-ternary
-          className={`mousetrap ${classes.input}`}
+          className={classNames('mousetrap', styles.numericInputInput)}
           value={value}
           onChange={this.handleChange}
         />
@@ -129,7 +129,6 @@ class NumericInput extends React.Component {
 }
 
 NumericInput.propTypes = {
-  classes: PropTypes.object.isRequired,
   arrows: PropTypes.bool.isRequired,
   value: PropTypes.number.isRequired,
   min: PropTypes.number.isRequired,
@@ -139,79 +138,4 @@ NumericInput.propTypes = {
   onChange: PropTypes.func.isRequired,
 };
 
-// ------ static styles ------
-
-const staticStyles = {
-  span: {
-    position: 'relative',
-    display: 'inline-block',
-    borderRadius: 0,
-    fontSize: 13,
-  },
-  input: {
-    padding: '3px 10px 3px 6px',
-    boxSizing: 'border-box',
-    height: 28,
-    width: 'auto',
-    // height: 28,
-    fontSize: 15,
-    borderRadius: 2,
-    fontFamily: uiConsts.fontFamily,
-    border: '1px solid rgb(204, 204, 204)',
-    display: 'block',
-    // -webkit-appearance: 'none',
-    lineHeight: 'normal',
-  },
-  b: {
-    position: 'absolute',
-    right: 2,
-    width: 15,
-    borderColor: 'rgba(0, 0, 0, 0.0980392)',
-    borderStyle: 'solid',
-    textAlign: 'center',
-    cursor: 'pointer',
-    transition: 'all 0.1s',
-    boxShadow: 'none',
-    '&:hover': {
-      background: 'rgba(0, 0, 0, 0.2)',
-      boxShadow: '0 0 0 0.5px rgba(0, 0, 0, 0.3)',
-    },
-  },
-  b1: {
-    top: 2,
-    bottom: '50%',
-    borderRadius: '2px 2px 0px 0px',
-    borderWidth: '1px 1px 0px',
-    background: 'rgba(0, 0, 0, 0.0980392)',
-    height: 12,
-  },
-  b2: {
-    top: '50%',
-    bottom: 2,
-    borderRadius: '0px 0px 2px 2px',
-    borderWidth: '0px 1px 1px',
-    background: 'rgba(0, 0, 0, 0.0980392)',
-    height: 12,
-  },
-  i: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    width: 0,
-    height: 0,
-  },
-  i1: {
-    borderWidth: '0px 0.6ex 0.6ex',
-    borderColor: 'transparent transparent rgba(0, 0, 0, 0.701961)',
-    borderStyle: 'solid',
-    margin: '-0.3ex 0px 0px -0.56ex',
-  },
-  i2: {
-    borderWidth: '0.6ex 0.6ex 0px',
-    borderColor: 'rgba(0, 0, 0, 0.701961) transparent transparent',
-    borderStyle: 'solid',
-    margin: '-0.3ex 0px 0px -0.56ex',
-  },
-};
-
-export default injectSheet(staticStyles)(NumericInput);
+export default NumericInput;
