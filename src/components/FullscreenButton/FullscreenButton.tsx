c3aa67cbf0fa470f7092ad5410e82b5bd729b1f2
dpenciso <dpenciso@msn.com>
2022-10-04 12:28:57 -0600

    typescripting

--- src/components/FullscreenButton/FullscreenButton.tsx
+++ src/components/FullscreenButton/FullscreenButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
