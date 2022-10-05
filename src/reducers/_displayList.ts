01ce3d2aa05885db65b36a9bd649a6a36aebe3d2
dpenciso <dpenciso@msn.com>
2022-10-05 11:18:10 -0600

    better typescripting

--- src/reducers/_displayList.ts
+++ src/reducers/_displayList.ts
@@ -7,7 +7,7 @@ const displayListReducer = (
     didInvalidate: false,
     list: [],
   },
-  action: { type: string; list: [Display]; receivedAt: number },
+  action: { type: string; list: Display[]; receivedAt: number },
 ) => {
   switch (action.type) {
     case REQUEST_DISPLAY_LIST:
