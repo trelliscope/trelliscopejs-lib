10b0a7ddda032b7c038fe3afe8f33552e59d04e7
dpenciso <dpenciso@msn.com>
2022-10-07 08:41:01 -0600

    159-convert-reducerssidebarjs-to-typescript

--- src/reducers/sidebar.ts
+++ src/reducers/sidebar.ts
@@ -1,6 +1,6 @@
 import { ACTIVE_SIDEBAR } from '../constants';
 
-const sidebarReducer = (state = { active: '' }, action) => {
+const sidebarReducer = (state = { active: '' }, action: { type: string; active: string; }) => {
   switch (action.type) {
     case ACTIVE_SIDEBAR:
       return { ...state, active: action.active === state.active ? '' : action.active };
