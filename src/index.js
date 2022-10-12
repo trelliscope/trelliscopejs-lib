dbaeecc4136f6a06bb090d9b579c108b55e528a9
Brandon McArthur <brandonmcarthur@Brandons-MacBook-Pro-3.local>
2022-10-12 16:31:40 -0600

    131: fixed index file

--- src/index.js
+++ src/index.js
@@ -227,13 +227,13 @@ window.trelliscopeApp = trelliscopeApp;
 // trelliscopeApp('02a6e2cc', '_test/univar/config.jsonp', { logger: true });
 // trelliscopeApp('001a3be8', '_test/foundationtest/config.jsonp', { logger: true });
 // trelliscopeApp('fcf74975', '_test/gapminder_autocogs/config.jsonp', { logger: true });
-// trelliscopeApp('80222985', '_test/gapminder_coggroups/config.jsonp', { logger: true });
+trelliscopeApp('80222985', '_test/gapminder_coggroups/config.jsonp', { logger: true });
 // trelliscopeApp('62e90658', '_test/gapminder_bells/config.jsonp', { logger: true });
 // trelliscopeApp('62e90658', '_test/trelliscope-examples2/gapminder_bells/config.jsonp', { logger: true });
 // trelliscopeApp('80222985', '_test/gapminder_coggroups/config.json', { logger: true });
 // trelliscopeApp('96c61ca5', '_test/trelliscope-examples2/gapminder_reldisp/config.jsonp', { logger: true });
 // trelliscopeApp('96c61ca5', '/config.json', { logger: true, mockData: true });
-trelliscopeApp('17a6ca23', '_test/trelliscope-examples2/network_nonraster/config.jsonp', { logger: true });
+// trelliscopeApp('17a6ca23', '_test/trelliscope-examples2/network_nonraster/config.jsonp', { logger: true });
 
 // trelliscopeApp('87203c56', '_test/error/config.jsonp', { logger: true });
 // trelliscopeApp('07ed5efb', '_test/error2/config.jsonp', { logger: true });
