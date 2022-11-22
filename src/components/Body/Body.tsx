b1d3625988f713b5184a2ff25a0a40b384f5445c
dpenciso <dpenciso@msn.com>
2022-11-22 03:37:13 -0700

    converts body.tsx to use redux hooks

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createSelector } from 'reselect';
+import { useSelector } from 'react-redux';
 import { windowWidthSelector, windowHeightSelector } from '../../selectors/ui';
 import SideButtons from '../SideButtons';
 import Sidebar from '../Sidebar';
 import Content from '../Content';
-import type { RootState } from '../../store';
 import styles from './Body.module.scss';
 
-interface BodyProps {
-  width: number;
-  height: number;
-}
+const Body: React.FC = () => {
+  const width = useSelector(windowWidthSelector);
+  const height = useSelector(windowHeightSelector);
+  return (
+    <div className={styles.body} style={{ width, height }}>
+      <SideButtons />
+      <Sidebar />
+      <Content />
+    </div>
+  );
+};
 
-const Body: React.FC<BodyProps> = ({ width, height }) => (
-  <div className={styles.body} style={{ width, height }}>
-    <SideButtons />
-    <Sidebar />
-    <Content />
-  </div>
-);
-
-// ------ redux container ------
-
-const stateSelector = createSelector(windowWidthSelector, windowHeightSelector, (width, height) => ({
-  width,
-  height,
-}));
-
-const mapStateToProps = (state: RootState) => stateSelector(state);
-
-export default connect(mapStateToProps)(Body);
+export default Body;
