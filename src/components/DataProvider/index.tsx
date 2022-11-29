2ed595c3630941f71a87c2595a3e1c5ce609e3f0
Jeff Mills <breathmills@gmail.com>
2022-11-29 16:55:09 -0700

    Add displayInfoAPI

---
+++ src/components/DataProvider/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import crossfilter from 'crossfilter2';
+import { useMetaData } from '../../slices/metaDataAPI';
+
+interface DataProviderProps {
+  children: React.ReactNode;
+  client: any;
+}
+
+const cf = crossfilter();
+
+window.crossfilter = cf;
+
+const DataContext = React.createContext({});
+
+const DataProvider: React.FC<DataProviderProps> = ({ children, client }) => {
+  const { data: metaData } = useMetaData();
+  const {};
+  if (metaData) {
+    cf.add(metaData);
+    console.log(metaData);
+  }
+  console.log('DataProvider', client);
+
+  return <DataContext.Provider value={{}}>{children}</DataContext.Provider>;
+};
+
+export default DataProvider;
