316031a578c9a6259ec26ca3159530813cd75f1f
Brandon McArthur <brandon_mcarthur@msn.com>
2022-12-06 13:42:13 -0700

    379-fullscreen: refactored fullscreen to use native browser fullscreen (#404)
    Co-authored-by: Brandon McArthur <brandonmcarthur@Brandons-MacBook-Pro-3.local>

--- src/components/FullscreenButton/FullscreenButton.tsx
+++ src/components/FullscreenButton/FullscreenButton.tsx
@@ -1,90 +1,57 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMaximize, faMinimize } from '@fortawesome/free-solid-svg-icons';
-import { useHotkeys } from 'react-hotkeys-hook';
 import { addClass, removeClass } from '../../classManipulation';
-import { dialogOpenSelector, fullscreenSelector, appIdSelector, singlePageAppSelector } from '../../selectors';
-import { origWidthSelector, origHeightSelector } from '../../selectors/ui';
+import { fullscreenSelector, singlePageAppSelector } from '../../selectors';
 import { setFullscreen } from '../../slices/appSlice';
 import { windowResize } from '../../slices/uiSlice';
+import { origHeightSelector, origWidthSelector } from '../../selectors/ui';
 import styles from './FullscreenButton.module.scss';
-
-interface NewDims {
-  width: number;
-  height: number;
-}
 
 const FullscreenButton: React.FC = () => {
   const dispatch = useDispatch();
-  const dialog = useSelector(dialogOpenSelector);
   const fullscreen = useSelector(fullscreenSelector);
-  const appId = useSelector(appIdSelector);
   const singlePageApp = useSelector(singlePageAppSelector);
   const ww = useSelector(origWidthSelector);
   const hh = useSelector(origHeightSelector);
-  let yOffset = window.pageYOffset;
+  const originalDims = { width: ww, height: hh };
 
-  const toggleFullscreen = (
-    fullscreenToggled: boolean,
-    fullscreenAppId: string,
-    appDims: { width: number; height: number },
-    fullscreenYOffset: number,
-  ) => {
-    const el = document.getElementById(fullscreenAppId) as HTMLElement;
-    const newDims = { width: 0, height: 0 } as NewDims;
-    if (fullscreenToggled) {
-      addClass(document.body, 'trelliscope-fullscreen-body');
-      addClass(document.getElementsByTagName('html')[0], 'trelliscope-fullscreen-html');
-      addClass(el, 'trelliscope-fullscreen-el');
-      newDims.width = window.innerWidth;
-      newDims.height = window.innerHeight;
-      // move the div to the outside of the document to make sure it's on top
-      const bodyEl = document.getElementById('trelliscope-fullscreen-div') as HTMLElement;
-      bodyEl.style.display = 'block';
-      bodyEl.appendChild(el);
-    } else {
-      removeClass(document.body, 'trelliscope-fullscreen-body');
-      removeClass(document.getElementsByTagName('html')[0], 'trelliscope-fullscreen-html');
-      removeClass(el, 'trelliscope-fullscreen-el');
-      newDims.width = appDims.width;
-      newDims.height = appDims.height;
-      // move the div back to its parent element
-      const parentId = document.getElementById(`${el.id}-parent`) as HTMLElement;
-      if (parentId) {
-        parentId.appendChild(el);
-      }
-      const fullscreenDiv = document.getElementById('trelliscope-fullscreen-div') as HTMLElement;
-      if (fullscreenDiv) {
-        fullscreenDiv.style.display = 'none';
-      }
-      // restore to y offset we were at before going fullscreen
-      window.scrollTo(window.pageXOffset, fullscreenYOffset);
+  const mainEl = document.getElementsByClassName('trelliscope-not-spa')[0] as HTMLElement;
+  const toggleFullScreen = () => {
+    if (!document.fullscreenElement) {
+      document.body.requestFullscreen();
+      addClass(mainEl, 'trelliscope-spa');
+      dispatch(setFullscreen(true));
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+      removeClass(mainEl, 'trelliscope-spa');
+      dispatch(setFullscreen(false));
+      dispatch(windowResize(originalDims));
     }
-    dispatch(setFullscreen(fullscreenToggled));
-    dispatch(windowResize(newDims));
   };
 
-  useHotkeys('esc', () => toggleFullscreen(false, appId, { width: ww, height: hh }, yOffset), {
-    enabled: fullscreen && !singlePageApp && !dialog,
-  });
+  const handleNativeKeyEsc = useCallback(() => {
+    if (!document.fullscreenElement) {
+      removeClass(mainEl, 'trelliscope-spa');
+      dispatch(setFullscreen(false));
+      dispatch(windowResize(originalDims));
+    }
+  }, [dispatch, mainEl]);
+
+  useEffect(() => {
+    document.addEventListener('fullscreenchange', handleNativeKeyEsc);
+    return () => {
+      document.removeEventListener('fullscreenchange', handleNativeKeyEsc);
+    };
+  }, [handleNativeKeyEsc]);
 
   if (singlePageApp) {
     return null;
   }
 
   return (
-    <button
-      type="button"
-      className={styles.fullscreenButton}
-      onClick={() => {
-        if (!fullscreen) {
-          // toggling to fullscreen
-          yOffset = window.pageYOffset;
-        }
-        toggleFullscreen(!fullscreen, appId, { width: ww, height: hh }, yOffset);
-      }}
-    >
+    <button type="button" className={styles.fullscreenButton} onClick={toggleFullScreen}>
       <div className={styles.fullscreenButtonIcon}>
         {!fullscreen ? <FontAwesomeIcon icon={faMaximize} size="lg" /> : <FontAwesomeIcon icon={faMinimize} size="lg" />}
       </div>
