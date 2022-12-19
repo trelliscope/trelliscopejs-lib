cb3e73664fcbe338ee049418d0a02b2b3be3f6e3
Brandon McArthur <brandonmcarthur@Brandons-MacBook-Pro-3.local>
2022-12-19 15:41:46 -0700

    459: removed unneeded elvis

--- src/selectors/cogData.ts
+++ src/selectors/cogData.ts
@@ -21,7 +21,7 @@ export const cogFiltDistSelector = createSelector(
       // for every active filter, calculate the conditional distribution
       const keys = filterView.active;
       for (let i = 0; i < keys.length; i += 1) {
-        if (cdi.info.cogInfo && cdi.info.cogInfo[keys[i]]?.type === 'factor') {
+        if (cdi.info.cogInfo && cdi.info.cogInfo[keys[i]].type === 'factor') {
           const orderValue = filter[keys[i]] && filter[keys[i]].orderValue ? filter[keys[i]].orderValue : 'ct,desc';
 
           let dist = [] as Grouping<string, number>[];
