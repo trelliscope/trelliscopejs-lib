60061102dd8bf5a6ebebb5d0fa226fdd8e3d47c8
Brandon McArthur <brandon_mcarthur@msn.com>
2023-01-05 09:45:58 -0700

    514-contentnew: fixed panels to render with any image type (#515)
    * 514-contentnew: fixed panels to render with any image type
    
    * 514: accounted for null panel formats
    
    * 514: removed mistaken import
    
    Co-authored-by: Brandon McArthur <brandonmcarthur@Brandons-MacBook-Pro-3.local>

--- src/components/ContentNew/ContentNew.tsx
+++ src/components/ContentNew/ContentNew.tsx
@@ -42,10 +42,14 @@ const ContentNew: React.FC = () => {
               <div key={getPanelImageName(displayInfo.key_cols, md)} className={styles.panel}>
                 <div className={styles.panelGraphic}>
                   <img
-                    src={`/${basePath}/displays/${snakeCase(displayInfo.name)}/panels/${getFileName(
-                      md[displayInfo.key_cols[1]],
-                      md[displayInfo.key_cols[0]],
-                    )}.svg`}
+                    src={
+                      displayInfo.panel_format !== null
+                        ? `/${basePath}/displays/${snakeCase(displayInfo.name)}/panels/${getFileName(
+                            md[displayInfo.key_cols[1]],
+                            md[displayInfo.key_cols[0]],
+                          )}.${displayInfo?.panel_format}`
+                        : getFileName(md[displayInfo.key_cols[1]], md[displayInfo.key_cols[0]])
+                    }
                     alt="display"
                   />
                 </div>
