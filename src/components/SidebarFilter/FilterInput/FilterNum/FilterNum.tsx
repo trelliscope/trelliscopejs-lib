acc0e5919d04d26bbde6c091bc4ea0c373be10f8
dpenciso <dpenciso@msn.com>
2023-01-20 11:39:22 -0700

    updates number filter range fields

--- src/components/SidebarFilter/FilterInput/FilterNum/FilterNum.tsx
+++ src/components/SidebarFilter/FilterInput/FilterNum/FilterNum.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import type { CSSProperties } from 'react';
 import { useDispatch } from 'react-redux';
+import { TextField } from '@mui/material';
 import { FILTER_TYPE_NUMBERRANGE } from '../../../../constants';
 import useMetaInfo from '../../../../selectors/useMetaInfo';
 import { addFilter, updateFilter } from '../../../../slices/filterSlice';
@@ -33,6 +35,60 @@ const FilterNum: React.FC<FilterNumProps> = ({ meta, filter = undefined }) => {
 
   if (!data?.length) return null;
 
+  const inputStyle = { textAlign: 'center', paddingBottom: 2 } as CSSProperties;
+
+  const { min, max } = filter;
+  if (max && min && max < min) {
+    inputStyle.color = 'red';
+  }
+
+  const rangeInput = {
+    width: 75,
+    marginTop: -5,
+    fontSize: 16,
+    transform: 'scale(0.85)',
+    transformOrigin: '0 0',
+  };
+
+  // const checkValidNumber = (which: string, lower: number | string = '', upper: number | string = '') => {
+  //   if (which === 'to') {
+  //     if (lower && parseFloat(lower.toString()) > parseFloat(upper.toString())) {
+  //       return false;
+  //     }
+  //   } else if (upper && parseFloat(upper.toString()) < parseFloat(lower?.toString())) {
+  //     return false;
+  //   }
+  //   return true;
+  // };
+
+  const handleInput = (val: string, which: string) => {
+    let newState = {} as INumberRangeFilterState;
+    const newVal = val === '' ? undefined : parseFloat(val);
+    const lower = which === 'min' ? newVal : filter?.min;
+    const upper = which === 'max' ? newVal : filter?.max;
+    if (lower === undefined && upper === undefined) {
+      newState = {
+        varname: filter.varname,
+        filtertype: 'numberrange',
+        type: filter.type,
+      };
+    } else {
+      newState = {
+        varname: filter.varname,
+        filtertype: 'numberrange',
+        type: filter.type,
+        min: lower,
+        max: upper,
+      };
+    }
+
+    dispatch(updateFilter(newState));
+  };
+
+  // calculate step value for numeric input
+  const hspan = (xDomain[1] - xDomain[0]) * xDomain.length;
+  const step = (10 ** Math.round(Math.log10(hspan / 100) - 0.4)) as number;
+
   return (
     <div className={styles.filterNum}>
       <div className={styles.filterNumChart}>
@@ -47,14 +103,33 @@ const FilterNum: React.FC<FilterNumProps> = ({ meta, filter = undefined }) => {
           defaultBrushValues={[filter?.min, filter?.max]}
         />
       </div>
-      <div className={styles.filterNumInput}>
-        {/* <TextField
-          placeholder="regex"
-          classes={{ root: styles.filterNumRegex }}
-          value={filter?.regexp || ''}
-          onChange={handleRegex}
+      <div className={styles.filterNumInputContainer}>
+        <div className={styles.filterNumRangeInputText}>Range:</div>
+        <TextField
+          name="minText"
+          style={rangeInput}
+          inputProps={{
+            style: inputStyle,
+            step,
+          }}
+          type="number"
+          value={filter?.min}
+          onChange={(e) => handleInput(e.target.value, 'min')}
           variant="standard"
-        /> */}
+        />
+        <div className={`${styles.filterNumRangeInputText} ${styles.filterNumRangeInputTextDash}`}>-</div>
+        <TextField
+          name="maxText"
+          style={rangeInput}
+          inputProps={{
+            style: inputStyle,
+            step,
+          }}
+          type="number"
+          value={filter?.max}
+          onChange={(e) => handleInput(e.target.value, 'max')}
+          variant="standard"
+        />
       </div>
     </div>
   );
