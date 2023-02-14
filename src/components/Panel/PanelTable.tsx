18ce4d2e02378a859d5326225c7f899efbc3e088
Brandon McArthur <brandonmcarthur@Brandons-MacBook-Pro-3.local>
2023-02-14 13:59:47 -0700

    635: added x to each panel to remove labels

--- src/components/Panel/PanelTable.tsx
+++ src/components/Panel/PanelTable.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch } from 'react-redux';
+import { faArrowUpRightFromSquare, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   INPUT_TYPE_RADIO,
@@ -13,6 +14,7 @@ import {
 import FormattedNumber from '../FormattedNumber';
 import { PanelInputText, PanelInputRadios } from '../PanelInputs';
 import PanelTableLabelCell from './PanelTableLabelCell';
+import { setLabels } from '../../slices/labelsSlice';
 import styles from './Panel.module.scss';
 
 interface PanelTableProps {
@@ -21,42 +23,55 @@ interface PanelTableProps {
   inputs: IInput[];
 }
 
-const PanelTable: React.FC<PanelTableProps> = ({ labels, data, inputs }) => (
-  <table className={styles.panelLabels} width="100%">
-    <tbody>
-      {inputs?.map((input) => (
-        <tr key={input.name} className={styles.panelLabel}>
-          <PanelTableLabelCell value={input.name} label={input.label} />
-          <td className={styles.panelLabelCell}>
-            {input.type === INPUT_TYPE_TEXT && (
-              <PanelInputText name={input.name} rows={(input as ITextInput).height} panelKey={data[PANEL_KEY] as string} />
-            )}
-            {input.type === INPUT_TYPE_RADIO && (
-              <PanelInputRadios
-                name={input.name}
-                options={(input as IRadioInput).options}
-                panelKey={data[PANEL_KEY] as string}
-              />
-            )}
-          </td>
-        </tr>
-      ))}
-      {labels.map((label) => (
-        <tr key={label.varname} className={styles.panelLabel}>
-          <PanelTableLabelCell value={label.varname} label={label.label} />
-          <td className={styles.panelLabelCell}>
-            {(label.type === META_TYPE_STRING || label.type === META_TYPE_FACTOR) && data[label.varname]}
-            {label.type === META_TYPE_NUMBER && <FormattedNumber value={data[label.varname] as number} />}
-            {label.type === META_TYPE_HREF && (
-              <a href={data[label.varname] as string} rel="noopener noreferrer" target="_blank">
-                <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-              </a>
-            )}
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+const PanelTable: React.FC<PanelTableProps> = ({ labels, data, inputs }) => {
+  const dispatch = useDispatch();
+  const handleLableRemove = (label: string) => {
+    const newLabels = labels.map((labelItem) => labelItem.varname).filter((labelItem) => labelItem !== label);
+    dispatch(setLabels(newLabels));
+  };
+
+  return (
+    <table className={styles.panelLabels} width="100%">
+      <tbody>
+        {inputs?.map((input) => (
+          <tr key={input.name} className={styles.panelLabel}>
+            <PanelTableLabelCell value={input.name} label={input.label} />
+            <td className={styles.panelLabelCell}>
+              {input.type === INPUT_TYPE_TEXT && (
+                <PanelInputText name={input.name} rows={(input as ITextInput).height} panelKey={data[PANEL_KEY] as string} />
+              )}
+              {input.type === INPUT_TYPE_RADIO && (
+                <PanelInputRadios
+                  name={input.name}
+                  options={(input as IRadioInput).options}
+                  panelKey={data[PANEL_KEY] as string}
+                />
+              )}
+            </td>
+          </tr>
+        ))}
+        {labels.map((label) => (
+          <tr key={label.varname} className={styles.panelLabel}>
+            <PanelTableLabelCell value={label.varname} label={label.label} />
+            <td className={styles.panelLabelCell}>
+              <div className={styles.panelLabelCellContent}>
+                {(label.type === META_TYPE_STRING || label.type === META_TYPE_FACTOR) && data[label.varname]}
+                {label.type === META_TYPE_NUMBER && <FormattedNumber value={data[label.varname] as number} />}
+                {label.type === META_TYPE_HREF && (
+                  <a href={data[label.varname] as string} rel="noopener noreferrer" target="_blank">
+                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                  </a>
+                )}
+                <button type="button" className={styles.panelLabelClose} onClick={() => handleLableRemove(label.varname)}>
+                  <FontAwesomeIcon icon={faXmark} />
+                </button>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default PanelTable;
