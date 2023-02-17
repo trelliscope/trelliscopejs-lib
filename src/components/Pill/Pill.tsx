98b80b5b575af699b891268aa27548410259690a
David Enciso <45321690+dpenciso@users.noreply.github.com>
2023-02-17 09:14:31 -0700

    adds active state to active filter pills (#659)
    * adds active state to active filter pills
    
    * refactor getting active filters

--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './Pill.module.scss';
 
 interface PillProps {
   children: React.ReactNode;
   onClick?: () => void;
+  curFiltersArr: string[];
 }
 
-const Pill: React.FC<PillProps> = ({ children, onClick }) => (
+const Pill: React.FC<PillProps> = ({ children, onClick, curFiltersArr }) => (
   <li className={styles.pill}>
-    <button type="button" className={styles.pillButton} onClick={onClick}>
+    <button
+      type="button"
+      className={classNames(styles.pillButton, {
+        [styles.pillButton__active]: curFiltersArr.includes(children as string),
+      })}
+      onClick={onClick}
+    >
       {children}
     </button>
   </li>
