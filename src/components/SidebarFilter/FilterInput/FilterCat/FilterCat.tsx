9218adb2250795c433678090ac0f61b3f68d4d78
Brandon McArthur <brandonmcarthur@Brandons-MacBook-Pro-3.local>
2023-03-08 15:39:29 -0700

    623-ellipses: inital commit adding ellipses to filtercat

--- src/components/SidebarFilter/FilterInput/FilterCat/FilterCat.tsx
+++ src/components/SidebarFilter/FilterInput/FilterCat/FilterCat.tsx
@@ -1,5 +1,8 @@
-import React, { useContext, useEffect } from 'react';
-import { TextField } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import type { SetStateAction } from 'react';
+import { TextField, IconButton, Menu, MenuItem } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch } from 'react-redux';
 import { FILTER_TYPE_CATEGORY } from '../../../../constants';
 import useMetaInfo from '../../../../selectors/useMetaInfo';
@@ -8,6 +11,7 @@ import CatHistogram from '../../../CatHistogram';
 import { DataContext } from '../../../DataProvider';
 
 import styles from './FilterCat.module.scss';
+import { useDisplayMetas } from '../../../../slices/displayInfoAPI';
 
 interface FilterCatProps {
   meta: IFactorMeta;
@@ -16,8 +20,56 @@ interface FilterCatProps {
 
 const FilterCat: React.FC<FilterCatProps> = ({ meta, filter }) => {
   const { domain = [0, 0], dist = {} } = useMetaInfo(meta.varname, meta.type);
+  const displayMetas = useDisplayMetas();
+  const curDisplayMeta = displayMetas.find((d) => d.varname === meta.varname);
+  const { filterSortOrder } = curDisplayMeta as IMeta;
   const { groupBy } = useContext(DataContext);
   const dispatch = useDispatch();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const defaultSort = filterSortOrder || 'ct,desc';
+  const [curSort, setCurSort] = useState(defaultSort);
+
+  const sortChartData = (sortOrder: string, data: { key: string | number; value: number }[]) => {
+    const [sortKey, sortDir] = sortOrder.split(',');
+    return data.sort((a, b) => {
+      if (sortKey === 'ct') {
+        return sortDir === 'asc' ? a.value - b.value : b.value - a.value;
+      }
+      return sortDir === 'asc'
+        ? (a.key as string).localeCompare(b.key as string)
+        : (b.key as string).localeCompare(a.key as string);
+    });
+  };
+
+  const [sortedData, setSortedData] = useState(() => {
+    if (meta) {
+      return defaultSort ? sortChartData(defaultSort, groupBy(meta.varname)) : groupBy(meta.varname);
+    }
+    return [];
+  });
+
+  const sortOptions = [
+    { payload: 'ct,asc', text: 'Order: count ascending' },
+    { payload: 'ct,desc', text: 'Order: count descending' },
+    { payload: 'id,asc', text: 'Order: label ascending' },
+    { payload: 'id,desc', text: 'Order: label descending' },
+  ];
+
+  const handleSortChange = (sortOrder: string) => {
+    setCurSort((sortOrder as IMeta['filterSortOrder']) || defaultSort);
+    const sorted = sortChartData(sortOrder, groupBy(meta.varname));
+    setSortedData(sorted);
+  };
+
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+  };
+
+  const handleMenuIconClick = (event: { currentTarget: SetStateAction<null> }) => {
+    setMenuOpen(!menuOpen);
+    setAnchorEl(menuOpen ? null : event.currentTarget);
+  };
 
   const handleBarClick = (value: string) => {
     if (filter) {
@@ -69,7 +121,7 @@ const FilterCat: React.FC<FilterCatProps> = ({ meta, filter }) => {
     <div className={styles.filterCat}>
       <div className={styles.filterCatChart}>
         <CatHistogram
-          data={groupBy(meta.varname)}
+          data={sortedData.length > 0 ? sortedData : sortChartData(defaultSort, groupBy(meta.varname))}
           allData={dist}
           domain={domain}
           actives={filter?.values || []}
@@ -80,14 +132,43 @@ const FilterCat: React.FC<FilterCatProps> = ({ meta, filter }) => {
           onClick={handleBarClick}
         />
       </div>
-      <div className={styles.filterCatInput}>
-        <TextField
-          placeholder="regex"
-          classes={{ root: styles.filterCatRegex }}
-          value={filter?.regexp || ''}
-          onChange={handleRegex}
-          variant="standard"
-        />
+      <div className={styles.filterCatInputContainer}>
+        <div>
+          <TextField
+            placeholder="regex"
+            classes={{ root: styles.filterCatRegex }}
+            value={filter?.regexp || ''}
+            onChange={handleRegex}
+            variant="standard"
+          />
+        </div>
+        <div>
+          {/* this is an issue with the iconButton in materialUi not having the type for an onClick in the props
+          // @ts-ignore */}
+          <IconButton
+            aria-label="more"
+            aria-controls="long-menu"
+            aria-haspopup="true"
+            onClick={handleMenuIconClick}
+            sx={{ width: 25, height: 25 }}
+          >
+            <FontAwesomeIcon icon={faEllipsisV} size="xs" />
+          </IconButton>
+          <Menu id="long-menu" open={menuOpen} anchorEl={anchorEl} onClose={handleMenuClose}>
+            {sortOptions.map((d) => (
+              <MenuItem
+                key={d.payload}
+                selected={d.payload === curSort}
+                onClick={() => {
+                  handleSortChange(d.payload);
+                  handleMenuClose();
+                }}
+              >
+                {d.text}
+              </MenuItem>
+            ))}
+          </Menu>
+        </div>
       </div>
     </div>
   );
