99fd34c420314d4f6d1b50dc6d756b9ce6929794
dpenciso <dpenciso@msn.com>
2023-03-27 07:56:54 -0600

    sends user to first step if they clear inputs in export input dialog

--- src/index.tsx
+++ src/index.tsx
@@ -219,7 +219,7 @@ window.trelliscopeApp = trelliscopeApp;
 // trelliscopeApp('96c61ca5', '_test/trelliscope-examples2/gapminder_reldisp/config.jsonp', { logger: true });
 // trelliscopeApp('17a6ca23', '_test/trelliscope-examples2/network_nonraster/config.jsonp', { logger: true });
 // trelliscopeApp('96c61ca5', '/config.json', { logger: true, mockData: true });
-// trelliscopeApp('9bdb39b4', '_test/trelliscope-examples3/gapminder_bells/config.jsonp', { logger: true });
+trelliscopeApp('9bdb39b4', '_test/trelliscope-examples3/gapminder_bells/config.jsonp', { logger: true });
 // trelliscopeApp('44c922eb', '_test/trelliscope-examples3/gapminder_reldisp/config.jsonp', { logger: true });
 // trelliscopeApp('3a3019d9', '_test/trelliscope-examples3/network_nonraster/config.jsonp', { logger: true });
 // trelliscopeApp('c060b1f7', '_test/trelliscope-examples3/mars/config.jsonp', { logger: true });
