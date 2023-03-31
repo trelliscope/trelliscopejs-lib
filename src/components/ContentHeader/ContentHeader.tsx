ff7f377b3295586e29f4c434b7c91f06be35e930
Jeff Mills <breathmills@gmail.com>
2023-03-31 15:55:32 -0600

    add basic new layout skeleton

---
+++ src/components/ContentHeader/ContentHeader.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './ContentHeader.module.scss';
+
+const ContentHeader: React.FC = () => <div className={styles.contentHeader}>ContentHeader</div>;
+
+export default ContentHeader;
