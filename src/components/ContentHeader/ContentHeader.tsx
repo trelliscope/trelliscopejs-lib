94ee79d4c61f56ddd3c87f919f624b13f39d4196
Jeff Mills <breathmills@gmail.com>
2023-04-07 10:32:00 -0600

    Add sidebar drawer

--- src/components/ContentHeader/ContentHeader.tsx
+++ src/components/ContentHeader/ContentHeader.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import CloseIcon from '@mui/icons-material/Close';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { sidebarActiveSelector } from '../../selectors/ui';
+import { setSidebarActive } from '../../slices/uiSlice';
 import styles from './ContentHeader.module.scss';
 
-const ContentHeader: React.FC = () => <div className={styles.contentHeader}>ContentHeader</div>;
+const ContentHeader: React.FC = () => {
+  const dispatch = useDispatch();
+  const sidebarOpen = useSelector(sidebarActiveSelector);
+
+  return (
+    <div className={styles.contentHeader}>
+      <div className={styles.contentHeaderControls}>
+        <div className={styles.contentHeaderControlsItem}>
+          <Button
+            onClick={() => dispatch(setSidebarActive(!sidebarOpen))}
+            variant={sidebarOpen ? 'contained' : 'text'}
+            sx={{
+              color: sidebarOpen ? '#fff' : '#000',
+              fontWeight: 600,
+              textTransform: 'unset',
+              fontSize: '15px',
+              borderRadius: 0,
+            }}
+            startIcon={sidebarOpen ? <CloseIcon /> : <KeyboardArrowLeftIcon />}
+          >
+            Explore
+          </Button>
+        </div>
+      </div>
+      <div className={styles.contentHeaderPagination} />
+    </div>
+  );
+};
 
 export default ContentHeader;
--- src/components/SidebarNew/SidebarNew.tsx
+++ src/components/SidebarNew/SidebarNew.tsx
@@ -1,15 +1,30 @@
 import { Drawer } from '@mui/material';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { sidebarActiveSelector } from '../../selectors/ui';
 import styles from './SidebarNew.module.scss';
 
+const drawerWidth = 400;
+
 const SidebarNew: React.FC = () => {
-  console.log('SidebarNew');
+  const sidebarOpen = useSelector(sidebarActiveSelector);
   return (
-    <>
-      <Drawer className={styles.sidebarNew} variant="persistent" anchor="left">
-        hello
-      </Drawer>
-    </>
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      open={sidebarOpen}
+      className={styles.sidebarNew}
+      variant="persistent"
+      anchor="left"
+    >
+      hello
+    </Drawer>
   );
 };
 
