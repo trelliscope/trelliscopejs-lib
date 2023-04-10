211a4663939ca8a0b4d966073c9400e73d26167b
Brandon McArthur <brandonmcarthur@Brandons-MacBook-Pro-3.local>
2023-04-10 10:37:36 -0600

    747-header: cleanded up layoutslice

--- src/slices/layoutSlice.ts
+++ src/slices/layoutSlice.ts
@@ -28,48 +28,41 @@ export const layoutSlice = createSlice({
   initialState,
   reducers: {
     setLayout: (state, action: PayloadAction<LayoutAction>) => {
+      // the view change occurs before we know how many rows there will be,
+      // but the view needs to know how many rows there will be to calculate the page number that the index will be on .
+      // The resize and compute functions are in the content component.
+
+      // const obj = { ...action.payload };
+      // const newState = { ...state, ...obj };
+      // if (state.viewtype !== newState.viewtype) {
+      //   const prevColCount = state.viewtype === 'table' ? state.ncol : 1;
+      //   const colCount = newState.viewtype === 'grid' ? newState.ncol : 1;
+      //   console.log('prevNRow::', state.nrow, 'prevColCount::', prevColCount, 'page::', state.page);
+      //   const prevPanelIndex = state.nrow * prevColCount * (state.page - 1) + 1;
+      //   console.log('prevPanelIndex::', prevPanelIndex);
+      //   const itemsPerPage = colCount * newState.nrow;
+      //   console.log('itemsPerPage::', itemsPerPage, 'newState.nrow::', newState.nrow, 'colCount::', colCount);
+      //   console.log('newPage::::', Math.ceil((prevPanelIndex + 1) / itemsPerPage));
+      //   obj.page = Math.ceil((prevPanelIndex + 1) / itemsPerPage);
+      //   // if (Number.isNaN(obj.page)) {
+      //   //   obj.page = 1;
+      //   // }
+      // }
       // if the layout change was to nrow / ncol
       // then we need to recompute page
-      // console.log('itemsPerPage', itemsPerPage);
-      // console.log('prevPanelIndex', prevPanelIndex);
-      // TODO fix refresh and hash
-      // TODO fix resizing on table
-      // the view change occurs before we know how many rows there will be, but the view needs to know how many rows there will be to calculate the page number that the index will be on.
-
       const obj = { ...action.payload };
-      const newState = { ...state, ...obj };
-      if (state.viewtype !== newState.viewtype) {
-        const prevColCount = state.viewtype === 'table' ? state.ncol : 1;
-        const colCount = newState.viewtype === 'grid' ? newState.ncol : 1;
-        console.log('prevNRow::', state.nrow, 'prevColCount::', prevColCount, 'page::', state.page);
-        const prevPanelIndex = state.nrow * prevColCount * (state.page - 1) + 1;
-        console.log('prevPanelIndex::', prevPanelIndex);
-        const itemsPerPage = colCount * newState.nrow;
-        console.log('itemsPerPage::', itemsPerPage, 'newState.nrow::', newState.nrow, 'colCount::', colCount);
-        console.log('newPage::::', Math.ceil((prevPanelIndex + 1) / itemsPerPage));
-        obj.page = Math.ceil((prevPanelIndex + 1) / itemsPerPage);
-        // if (Number.isNaN(obj.page)) {
-        //   obj.page = 1;
-        // }
+      if (obj.nrow || obj.ncol) {
+        const prevPanelIndex = state.nrow * state.ncol * (state.page - 1) + 1;
+        if (obj.nrow) {
+          obj.page = Math.ceil(prevPanelIndex / (obj.nrow * state.ncol));
+        }
+        if (obj.ncol) {
+          obj.page = Math.ceil(prevPanelIndex / (state.nrow * obj.ncol));
+        }
+        if (Number.isNaN(obj.page)) {
+          obj.page = 1;
+        }
       }
-      // setLayout: (state, action: PayloadAction<LayoutAction>) => {
-      //   // if the layout change was to nrow / ncol
-      //   // then we need to recompute page
-      //   const obj = { ...action.payload };
-      //   if (obj.nrow || obj.ncol) {
-      //     const prevPanelIndex = state.nrow * state.ncol * (state.page - 1) + 1;
-      //     if (obj.nrow) {
-      //       obj.page = Math.ceil(prevPanelIndex / (obj.nrow * state.ncol));
-      //     }
-      //     if (obj.ncol) {
-      //       obj.page = Math.ceil(prevPanelIndex / (state.nrow * obj.ncol));
-      //     }
-      //     if (Number.isNaN(obj.page)) {
-      //       obj.page = 1;
-      //     }
-      //   }
-      //   return { ...state, ...obj };
-      // },
       return { ...state, ...obj };
     },
   },
