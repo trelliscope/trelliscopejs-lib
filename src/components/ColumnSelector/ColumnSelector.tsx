255f3252e5ad0f8d2790eb15feb309598e53db51
Brandon McArthur <brandonmcarthur@Brandons-MacBook-Pro-3.local>
2023-04-11 14:09:35 -0600

    747-header: refactor and added column selector

---
+++ src/components/ColumnSelector/ColumnSelector.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { TextField } from '@mui/material';
+import { selectLayout, setLayout } from '../../slices/layoutSlice';
+import styles from './ColumnSelector.module.scss';
+
+const ColumnSelector: React.FC = () => {
+  const dispatch = useDispatch();
+  const layout = useSelector(selectLayout);
+
+  const handleColumnChange = (value: string) => {
+    let nonZeroValue = value;
+    if (value.charAt(0) === '0') {
+      nonZeroValue = value.slice(1);
+    }
+    const num = parseInt(nonZeroValue || '1', 10);
+    if (num > 15 || num < 0) return;
+    dispatch(setLayout({ ncol: num }));
+  };
+
+  return (
+    <>
+      <div>Columns:</div>
+      <TextField
+        sx={{ maxWidth: 50, minWidth: 30, pt: 0.6 }}
+        variant="standard"
+        type="number"
+        size="small"
+        inputProps={{
+          inputMode: 'numeric',
+          pattern: '[0-9]*',
+          style: { marginBottom: -4, textAlign: 'center' },
+        }}
+        value={layout.ncol}
+        onChange={(e) => handleColumnChange(e.target.value)}
+      />
+    </>
+  );
+};
+
+export default ColumnSelector;
--- src/components/ContentHeader/ContentHeader.tsx
+++ src/components/ContentHeader/ContentHeader.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import classNames from 'classnames';
 import { Button } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import CloseIcon from '@mui/icons-material/Close';
@@ -6,96 +7,45 @@ import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import { sidebarActiveSelector } from '../../selectors/ui';
 import { setSidebarActive } from '../../slices/uiSlice';
 import { useSelectedDisplay } from '../../slices/selectedDisplaySlice';
-import { selectNumPerPage, selectPage, setLayout } from '../../slices/layoutSlice';
 import Pagination from '../Pagination';
-import { DataContext } from '../DataProvider';
+import ColumnSelector from '../ColumnSelector/ColumnSelector';
+import { selectLayout } from '../../slices/layoutSlice';
+
 import styles from './ContentHeader.module.scss';
-import { cogDataSelector, fullscreenSelector } from '../../selectors';
-import { selectDialogOpen } from '../../selectors/app';
 
 const ContentHeader: React.FC = () => {
   const selectedDisplay = useSelectedDisplay();
-  const { allData, filteredData } = useContext(DataContext);
   const dispatch = useDispatch();
+  const layout = useSelector(selectLayout);
   const displayLoaded = selectedDisplay?.name !== '';
-  const n = useSelector(selectPage);
-  const totPanels = filteredData.length;
-  const npp = useSelector(selectNumPerPage);
-  const fullscreen = useSelector(fullscreenSelector);
-  const cogData = useSelector(cogDataSelector);
-  const totPages = Math.ceil(totPanels / npp);
-  const dialogOpen = useSelector(selectDialogOpen);
   const sidebarOpen = useSelector(sidebarActiveSelector);
-
-  const handleChange = (page: number) => {
-    dispatch(
-      setLayout({
-        page,
-        type: 'layout',
-      }),
-    );
-  };
-
-  const pageLeft = () => {
-    let nn = n - 1;
-    if (nn < 1) {
-      nn += 1;
-    }
-    return handleChange(nn);
-  };
-
-  const pageRight = () => {
-    let nn = n + 1;
-    if (nn > totPages) {
-      nn -= 1;
-    }
-    return handleChange(nn);
-  };
-
-  const pageFirst = () => {
-    handleChange(1);
-  };
-
-  const pageLast = () => {
-    handleChange(totPages);
-  };
 
   return (
     <div className={styles.contentHeader}>
       <div className={styles.contentHeaderControls}>
-        <div className={styles.contentHeaderControlsItem}>
-          <Button
-            onClick={() => dispatch(setSidebarActive(!sidebarOpen))}
-            variant={sidebarOpen ? 'contained' : 'text'}
-            sx={{
-              color: sidebarOpen ? '#fff' : '#000',
-              fontWeight: 600,
-              textTransform: 'unset',
-              fontSize: '15px',
-              borderRadius: 0,
-            }}
-            startIcon={sidebarOpen ? <CloseIcon /> : <KeyboardArrowLeftIcon />}
-          >
-            Explore
-          </Button>
+        <div className={styles.contentHeaderControlsLeft}>
+          <div className={classNames(styles.contentHeaderControlsItem, styles.contentHeaderControlsItemToggle)}>
+            <Button
+              onClick={() => dispatch(setSidebarActive(!sidebarOpen))}
+              variant={sidebarOpen ? 'contained' : 'text'}
+              sx={{
+                color: sidebarOpen ? '#fff' : '#000',
+                textTransform: 'unset',
+                fontSize: '15px',
+                borderRadius: 0,
+              }}
+              startIcon={sidebarOpen ? <CloseIcon /> : <KeyboardArrowLeftIcon />}
+            >
+              Explore
+            </Button>
+          </div>
+          {layout?.viewtype !== 'table' && (
+            <div className={styles.contentHeaderControlsItem}>
+              <ColumnSelector />
+            </div>
+          )}
         </div>
-      </div>
-      <div className={styles.contentHeaderPagination}>
-        {displayLoaded && (
-          <Pagination
-            n={n}
-            totPanels={totPanels}
-            npp={npp}
-            dialogOpen={dialogOpen}
-            fullscreen={fullscreen}
-            cogData={cogData}
-            totPages={totPages}
-            pageLeft={pageLeft}
-            pageRight={pageRight}
-            pageFirst={pageFirst}
-            pageLast={pageLast}
-          />
-        )}
+        <div className={styles.contentHeaderControlsPagination}>{displayLoaded && <Pagination />}</div>
       </div>
     </div>
   );
