56208ccdc0bdf38c5eecf8e41193acf9a6d15db8
Brandon McArthur <brandonmcarthur@MacBook-Pro-2.local>
2023-08-11 17:07:10 -0600

    tests for layout switching

---
+++ tests/layoutselection.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+});
+
+test('layout selector is present with options', async ({ page }) => {
+  await page.getByTestId('layout-selector').click();
+  await expect(page.getByTestId('grid-select')).toBeVisible();
+  await expect(page.getByTestId('table-select')).toBeVisible();
+});
+
+test('layout selector changes layout to grid and panels are visible', async ({ page }) => {
+  await page.getByTestId('layout-selector').click();
+  await page.getByTestId('table-select').click();
+  await page.getByTestId('layout-selector').click();
+  await page.getByTestId('grid-select').click();
+  await expect(page.getByTestId('panel-content')).toBeVisible();
+});
+
+test('layout selector changes layout to table and rows are visible', async ({ page }) => {
+  await page.getByTestId('layout-selector').click();
+  await page.getByTestId('table-select').click();
+  await expect(page.getByTestId('table-content')).toBeVisible();
+});
