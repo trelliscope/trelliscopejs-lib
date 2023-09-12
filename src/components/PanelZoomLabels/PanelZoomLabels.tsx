5641b2b807dd0a302b705eb52143de04ea5b69d1
Brandon McArthur <brandonmcarthur@MacBook-Pro-2.local>
2023-09-11 19:45:14 -0600

    1088-labels: labels refactor in dialog

--- src/components/PanelZoomLabels/PanelZoomLabels.tsx
+++ src/components/PanelZoomLabels/PanelZoomLabels.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { faArrowUpRightFromSquare, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import classNames from 'classnames';
-import { Tooltip } from '@mui/material';
+import { useSelector } from 'react-redux';
 import {
   INPUT_TYPE_CHECKBOX,
   INPUT_TYPE_MULTISELECT,
@@ -10,29 +7,21 @@ import {
   INPUT_TYPE_RADIO,
   INPUT_TYPE_SELECT,
   INPUT_TYPE_TEXT,
-  META_TYPE_CURRENCY,
-  META_TYPE_DATE,
-  META_TYPE_DATETIME,
   META_TYPE_FACTOR,
-  META_TYPE_HREF,
-  META_TYPE_NUMBER,
-  META_TYPE_PANEL,
-  META_TYPE_STRING,
-  MISSING_TEXT,
-  PANEL_KEY,
 } from '../../constants';
-import FormattedNumber from '../FormattedNumber';
 import {
   PanelInputText,
   PanelInputRadios,
-  PanelInputMultiSelect,
   PanelInputSelect,
   PanelInputCheckbox,
+  PanelInputMultiSelect,
 } from '../PanelInputs';
 import PanelZoomLabelsCell from './PanelZoomLabelsCell';
 import { getLabelFromFactor } from '../../utils';
 import { useDisplayInfo, useDisplayMetas } from '../../slices/displayInfoAPI';
 import styles from './PanelZoomLabels.module.scss';
+import { selectLabels } from '../../slices/labelsSlice';
+import PanelZoomLabelsCellContent from './PanelZoomLabelsCellContent';
 
 interface PanelZoomLabelsProps {
   data: Datum;
@@ -43,6 +32,10 @@ interface PanelZoomLabelsProps {
 const PanelZoomLabels: React.FC<PanelZoomLabelsProps> = ({ labels, data, inputs }) => {
   const displayMetas = useDisplayMetas();
   const { data: displayInfo } = useDisplayInfo();
+  const stateLabels = useSelector(selectLabels);
+
+  const selectedLabels = labels.filter((label) => stateLabels.includes(label.varname));
+  const nonSelectedLabels = labels.filter((label) => !stateLabels.includes(label.varname));
 
   const panelKeyArr = displayInfo?.keycols?.map((keycol) => {
     const foundMeta = displayMetas.find((meta) => meta.varname === keycol);
@@ -54,19 +47,14 @@ const PanelZoomLabels: React.FC<PanelZoomLabelsProps> = ({ labels, data, inputs 
 
   const panelKey = panelKeyArr?.join('_');
 
-  //  are we okay with how the multiselect drop down moves now since its tied to the pencil and the drawer kinda shifts?
-  // for the sorting and the inputs, getting them in is doable, sorting might get a little odd since the inputs currently
-  // have a different state structure and sorting goes into crossfilter. This would probably be its own task that will take some time.
-
-  const getMetaLevels = (varname: string) => {
-    const foundMeta = displayMetas.find((meta) => meta.varname === varname);
-    return foundMeta?.levels;
-  };
   return (
     <table className={styles.panelZoomLabels} width="100%">
       <tbody>
+        <th colSpan={2} className={styles.panelZoomLabelsRow__title}>
+          Inputs
+        </th>
         {inputs?.map((input) => (
-          <tr key={input.name} className={classNames(styles.panelZoomLabelsRow, styles.panelZoomLabelsRow__input)}>
+          <tr key={input.name} className={styles.panelZoomLabelsRow}>
             <PanelZoomLabelsCell value={input.name} label={input.label} />
             <td className={styles.panelZoomLabelsCell}>
               <div className={styles.panelZoomLabelsCellContent}>
@@ -111,7 +99,10 @@ const PanelZoomLabels: React.FC<PanelZoomLabelsProps> = ({ labels, data, inputs 
             </td>
           </tr>
         ))}
-        {labels.map((label) => (
+        <th colSpan={2} className={styles.panelZoomLabelsRow__title}>
+          Selected Labels
+        </th>
+        {selectedLabels.map((label) => (
           <tr
             key={label.varname}
             className={
@@ -121,50 +112,23 @@ const PanelZoomLabels: React.FC<PanelZoomLabelsProps> = ({ labels, data, inputs 
             }
           >
             <PanelZoomLabelsCell value={label.varname} label={label.label} />
-            <td className={styles.panelZoomLabelsCell}>
-              <div className={styles.panelZoomLabelsCellContent}>
-                <Tooltip
-                  title={
-                    label.type === META_TYPE_FACTOR
-                      ? getLabelFromFactor(data[label.varname] as number, getMetaLevels(label.varname) as string[])
-                      : data[label.varname]
-                  }
-                  placement="left"
-                  arrow
-                >
-                  <div className={styles.panelZoomLabelsCellContentText}>
-                    {label.type !== META_TYPE_FACTOR && !data[label.varname] && data[label.varname] !== 0 && MISSING_TEXT}
-                    {label.type === META_TYPE_FACTOR &&
-                      getLabelFromFactor(data[label.varname] as number, getMetaLevels(label.varname) as string[])}
-                    {(label.type === META_TYPE_STRING || label.type === META_TYPE_DATE || label.type === META_TYPE_PANEL) &&
-                      data[label.varname]}
-                    {label.type === META_TYPE_DATETIME && data[label.varname]?.toString().replace('T', ' ')}
-                    {(label.type === META_TYPE_NUMBER || label.type === META_TYPE_CURRENCY) &&
-                      data[label.varname] &&
-                      (label.locale ? (
-                        <FormattedNumber
-                          value={data[label.varname] as number}
-                          isCurrency={label.type === META_TYPE_CURRENCY}
-                          currencyCode={label.code}
-                          maximumFractionDigits={label.digits}
-                        />
-                      ) : (
-                        <span>{data[label.varname]}</span>
-                      ))}
-                    {label.type === META_TYPE_HREF && data[label.varname] && (
-                      <a
-                        className={styles.panelZoomLabelsCellLink}
-                        href={data[label.varname] as string}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                      </a>
-                    )}
-                  </div>
-                </Tooltip>
-              </div>
-            </td>
+            <PanelZoomLabelsCellContent data={data} label={label} />
+          </tr>
+        ))}
+        <th colSpan={2} className={styles.panelZoomLabelsRow__title}>
+          Additional Labels
+        </th>
+        {nonSelectedLabels.map((label) => (
+          <tr
+            key={label.varname}
+            className={
+              !data[label.varname] && data[label.varname] !== 0
+                ? `${styles.panelZoomLabelsRow} ${styles.panelZoomLabelsRowMissing}`
+                : styles.panelZoomLabelsRow
+            }
+          >
+            <PanelZoomLabelsCell value={label.varname} label={label.label} />
+            <PanelZoomLabelsCellContent data={data} label={label} />
           </tr>
         ))}
       </tbody>
--- src/components/PanelZoomLabels/PanelZoomLabelsCell.tsx
+++ src/components/PanelZoomLabels/PanelZoomLabelsCell.tsx
@@ -8,12 +8,12 @@ interface PanelZoomLabelsCellProps {
 }
 
 const PanelZoomLabelsCell: React.FC<PanelZoomLabelsCellProps> = ({ value, label }) => (
-  <td style={{ padding: value !== label ? '0 12px' : '6px' }} className={styles.panelZoomLabelsCell}>
+  <td style={{ padding: value !== label ? '0 12px' : '8.5px 12px' }} className={styles.panelZoomLabelsCell}>
     {label ? (
       <Tooltip title={label} placement="left" arrow>
         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
           <span>{value}</span>
-          <span>{label}</span>
+          {label !== value && <span className={styles.panelZoomLabelsLabel}>{label}</span>}
         </Box>
       </Tooltip>
     ) : (
