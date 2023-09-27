fb276a03d0f97d2199e167b1e1448ce0b3c9a2f3
Brandon McArthur <brandonmcarthur@MacBook-Pro-2.local>
2023-09-27 11:06:41 -0600

    1052: added display state saving when switching displays

--- src/components/DisplaySelect/DisplaySelect.tsx
+++ src/components/DisplaySelect/DisplaySelect.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import type { MouseEvent } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { IconButton, Menu, MenuItem } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
-import { clearFilters, setFilterView, setFiltersandFilterViews } from '../../slices/filterSlice';
-import { setSort } from '../../slices/sortSlice';
-import { setLabels } from '../../slices/labelsSlice';
-import { setLayout } from '../../slices/layoutSlice';
+import { clearFilters, selectFilterState, setFilterView, setFiltersandFilterViews } from '../../slices/filterSlice';
+import { selectSort, setSort } from '../../slices/sortSlice';
+import { selectLabels, setLabels } from '../../slices/labelsSlice';
+import { selectLayout, setLayout } from '../../slices/layoutSlice';
 import type { LayoutAction } from '../../slices/layoutSlice';
 import { setSelectedDisplay, useSelectedDisplay } from '../../slices/selectedDisplaySlice';
 import { setRelDispPositions } from '../../slices/relDispPositionsSlice';
 import { useDisplayList } from '../../slices/displayListAPI';
 import { useDisplayInfo } from '../../slices/displayInfoAPI';
 import styles from './DisplaySelect.module.scss';
+import { useStoredInputValue, getLocalStorageKey } from '../../inputUtils';
+import { filterViewSelector } from '../../selectors';
 
 const DisplaySelect: React.FC = () => {
   const dispatch = useDispatch();
@@ -23,6 +25,14 @@ const DisplaySelect: React.FC = () => {
   const { data: displayList } = useDisplayList();
   const { data: displayInfo } = useDisplayInfo();
   const [isOpen, setIsOpen] = useState(false);
+  const STORED_NAME = 'trelliscope_display_switch_state';
+  const { setStoredValue } = useStoredInputValue(STORED_NAME, '');
+
+  const filterViews = useSelector(filterViewSelector);
+  const filters = useSelector(selectFilterState);
+  const sorts = useSelector(selectSort);
+  const labels = useSelector(selectLabels);
+  const layout = useSelector(selectLayout);
 
   const stateLayout = displayInfo?.state?.layout;
   const stateLabels = displayInfo?.state?.labels?.varnames;
@@ -30,9 +40,35 @@ const DisplaySelect: React.FC = () => {
   const stateFilters = displayInfo?.state?.filter;
   const activeDisplayName = displayInfo?.name;
 
-  // This is needed to make sure the default state is applied when switching to a new display
+  // This is needed to make sure the default state or the state from local storage that the user was previously on is applied when switching to a new display
   useEffect(() => {
     if (selectedDisplayName === activeDisplayName) {
+      const localSavedState = localStorage.getItem(getLocalStorageKey([], displayInfo?.name || '', STORED_NAME, ''));
+
+      if (localSavedState) {
+        const {
+          filter: valueFilter,
+          labels: valueLabels,
+          layout: valueLayout,
+          sort: valueSort,
+          filterView: valueFilterView,
+        } = JSON.parse(localSavedState).state;
+
+        if (valueLayout) dispatch(setLayout(valueLayout as LayoutAction));
+
+        if (valueLabels) dispatch(setLabels(valueLabels.varnames));
+
+        if (valueSort) dispatch(setSort(valueSort));
+
+        if (valueFilter) dispatch(setFiltersandFilterViews(valueFilter));
+
+        if (valueFilterView) {
+          const inactiveFilters = filterViews.inactive.filter((filter) => !valueFilterView.includes(filter));
+          dispatch(setFilterView({ name: { active: valueFilterView, inactive: inactiveFilters }, which: 'set' }));
+        }
+
+        return;
+      }
       dispatch(setLayout({ ...(stateLayout as LayoutAction), panel: '' }));
       dispatch(setLabels(stateLabels as string[]));
       dispatch(setFiltersandFilterViews(stateFilters as IFilterState[]));
@@ -49,8 +85,23 @@ const DisplaySelect: React.FC = () => {
     setAnchorEl(event.currentTarget);
     setIsOpen(true);
   };
-
   const handleClick = (name: string) => {
+    // Need to save to local storage a "view" of the current display before switching
+    // If a view is saved we want to load that in the new display
+    // Otherwise we want to load the default state of the new display
+    setStoredValue(
+      JSON.stringify({
+        name: activeDisplayName,
+        state: {
+          layout,
+          labels: { varnames: labels, type: 'labels' },
+          sort: sorts,
+          filter: filters,
+          filterView: filterViews?.active,
+        },
+      }),
+    );
+
     // need to clear out state for new display...
     dispatch(setFilterView({ name: '', which: 'removeActive' }));
     dispatch(clearFilters());
