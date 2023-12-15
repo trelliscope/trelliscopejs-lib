fef6249d0fff3a6dad6fc9d6a34c1db7da893fa5
Brandon McArthur <brandon_mcarthur@msn.com>
2023-12-15 08:04:45 -0700

    tests for labels (#1193)
    Co-authored-by: Brandon McArthur <brandonmcarthur@MacBook-Pro-2.local>

---
+++ tests/labels.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+});
+
+test('labels modal open and closes', async ({ page }) => {
+  await expect(page.getByTestId('variable-picker')).not.toBeVisible();
+  await page.getByTestId('labels-button').click();
+  await expect(page.getByTestId('variable-picker')).toBeVisible();
+  await page.click('body');
+  await expect(page.getByTestId('variable-picker')).not.toBeVisible();
+});
+
+test('label can be selected and deselected', async ({ page }) => {
+  await expect(page.getByTestId('variable-picker')).not.toBeVisible();
+  await expect(page.getByRole('row', { name: 'Set number 31203-1' }).locator('span')).not.toBeVisible();
+  await page.getByTestId('labels-button').click();
+  await expect(page.getByTestId('variable-picker')).toBeVisible();
+  await page.getByRole('option', { name: 'set_num Set number' }).getByRole('checkbox').click();
+  await page.click('body');
+  await expect(page.getByRole('row', { name: 'Set number 31203-1' }).locator('span')).toBeVisible();
+  await page.getByTestId('labels-button').click();
+  await expect(page.getByTestId('variable-picker')).toBeVisible();
+  await page.getByRole('option', { name: 'set_num Set number' }).getByRole('checkbox').click();
+  await page.click('body');
+  await expect(page.getByRole('row', { name: 'Set number 31203-1' }).locator('span')).not.toBeVisible();
+});
