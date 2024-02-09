9215065c00308af9e3490e25c63496f0a42948ba
Kyle McArthur <kyle@codeforgesolutions.com>
2024-02-09 14:29:47 -0700

    firefox-loading-issue: Fix issue with race condition leading to data … (#1203)
    * firefox-loading-issue: Fix issue with race condition leading to data not populating occasionaly on firefox
    
    * firefox-loading-issue: fix issue with switching views
    
    ---------
    
    Co-authored-by: kyle mcarthur <kmcarthur@mail.weber.edu>

--- src/selectors/app.ts
+++ src/selectors/app.ts
@@ -8,3 +8,5 @@ export const selectDialogOpen = (state: RootState) => state.app.dialog;
 export const selectErrorMessage = (state: RootState) => state.app.errorMsg;
 export const selectPanelDialog = (state: RootState) => state.app.panelDialog;
 export const panelDialogIsOpenSelector = (state: RootState) => state.app.panelDialog.open;
+export const selectMetaData = (state: RootState) => state.app.metaData;
+export const selectMetaDataState = (state: RootState) => state.app.metaDataState;
