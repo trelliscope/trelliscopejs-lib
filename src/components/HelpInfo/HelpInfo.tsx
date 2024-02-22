1871f9c8f6a5482d3cb4947824d1e56a0b885108
Brandon McArthur <brandon_mcarthur@msn.com>
2024-02-22 14:47:30 -0700

    ability to remove trelliscope wording (#1213)
    * ability to remove trelliscope wording
    
    * Minor changes for additional trelliscope verbage
    
    ---------
    
    Co-authored-by: Brandon McArthur <brandonmcarthur@MacBook-Pro-2.local>

--- src/components/HelpInfo/HelpInfo.tsx
+++ src/components/HelpInfo/HelpInfo.tsx
@@ -4,23 +4,14 @@ import { useHotkeys } from 'react-hotkeys-hook';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  IconButton,
-  Tab,
-  Tabs,
-} from '@mui/material';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Tab, Tabs } from '@mui/material';
 import { fullscreenSelector } from '../../selectors';
 import HowToUse from '../HowToUse';
 import Shortcuts from '../Shortcuts';
 import Credits from '../Credits';
 import styles from './HelpInfo.module.scss';
 import { useConfig } from '../../slices/configAPI';
+import { BACK } from '../../constants';
 
 const HelpInfo: React.FC = () => {
   const fullscreen = useSelector(fullscreenSelector);
@@ -40,13 +31,27 @@ const HelpInfo: React.FC = () => {
   useHotkeys('h', handleToggle, { enabled: fullscreen }, [open]);
   useHotkeys('esc', () => setOpen(false), { enabled: open });
 
+  console.log('config:::', configObj);
+  console.log('configObj.config1:::', configObj && configObj.config1 !== BACK.OUTATIME);
+
   return (
-    <div className={styles.helpInfoIcon}>
-      <IconButton data-testid="help-button" id="help-control" color="inherit" size="small" onClick={handleToggle}>
+    <Box
+      sx={{
+        marginTop: configObj && configObj.config1 !== BACK.OUTATIME ? '-9px' : '-2px',
+        opacity: configObj && configObj.config1 !== BACK.OUTATIME ? 0.7 : 1,
+      }}
+      className={styles.helpInfoIcon}
+    >
+      <IconButton
+        data-testid="help-button"
+        id="help-control"
+        color="inherit"
+        size={configObj && configObj.config1 !== BACK.OUTATIME ? 'small' : 'medium'}
+        onClick={handleToggle}
+      >
         <FontAwesomeIcon
           color={configObj?.theme?.isLightTextOnDark ? configObj?.theme?.lightText : configObj?.theme?.darkText}
           icon={faCircleQuestion}
-          size="sm"
         />
       </IconButton>
       <Dialog
@@ -58,25 +63,27 @@ const HelpInfo: React.FC = () => {
         maxWidth="md"
         data-testid="help-modal"
       >
-        <DialogTitle
-          id="dialog-viewer-title"
-          sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
-        >
-          <Box>
-            <div>{`Trelliscope v${window.__VERSION__}`}</div>
-            <div className={styles.helpInfoDialogWebsite}>
-              Learn more at{' '}
-              <a href="https://trelliscope.org" target="_blank" rel="noopener noreferrer">
-                trelliscope.org
-              </a>
-            </div>
-          </Box>
-        </DialogTitle>
+        {configObj && configObj.config1 !== BACK.OUTATIME && (
+          <DialogTitle
+            id="dialog-viewer-title"
+            sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
+          >
+            <Box>
+              <div>{`Trelliscope v${window.__VERSION__}`}</div>
+              <div className={styles.helpInfoDialogWebsite}>
+                Learn more at{' '}
+                <a href="https://trelliscope.org" target="_blank" rel="noopener noreferrer">
+                  trelliscope.org
+                </a>
+              </div>
+            </Box>
+          </DialogTitle>
+        )}
         <DialogContent>
           <Tabs value={tabNumber} onChange={handleChange}>
             <Tab data-testid="how-to-tab" label="How to Use" />
             <Tab data-testid="shortcuts-tab" label="Shortcuts" />
-            <Tab data-testid="credits-tab" label="Credits" />
+            {configObj && configObj.config1 !== BACK.OUTATIME && <Tab data-testid="credits-tab" label="Credits" />}
           </Tabs>
           {tabNumber === 0 && <HowToUse />}
           {tabNumber === 1 && <Shortcuts />}
@@ -88,7 +95,7 @@ const HelpInfo: React.FC = () => {
           </Button>
         </DialogActions>
       </Dialog>
-    </div>
+    </Box>
   );
 };
 
